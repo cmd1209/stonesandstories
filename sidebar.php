aaf7f6520ac34a0a854f2f10e9633de3d7db4659
Chad Danford <chad.m.danford@gmail.com>
2013-09-02 12:48:40 +0200

    Home php directory link

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 	    		<div class="box">
 	    			<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
 	    				<figure>
-	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
+	    					<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
 	    				</figure>
 	    			</a>		
 	    		</div>
@@ -16,11 +16,6 @@
 	    	<div class="sidemodule">
 	    		<div class="box">
 	    			<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="">
-	    		</div>
-	    	</div>
-	    	<div class="sidemodule ">
-	    		<div class="box">
-	    			<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/humbolt.jpg" alt="">
 	    		</div>
 	    	</div>
 <!-- /sidebar -->
