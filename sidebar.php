4c433f73d2d62af270bd5e1913dfc5f1d90b7adf
Chad Danford <chad.m.danford@gmail.com>
2013-09-02 12:39:39 +0200

    Humboldt Lab installed and activated

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,11 @@
 <!-- sidebar -->
 	    	<div class="sidemodule">
 	    		<div class="box">
-	    			<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/swam.jpg" alt="">
+	    			<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
+	    				<figure>
+	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
+	    				</figure>
+	    			</a>		
 	    		</div>
 	    	</div>
 	    	<div class="sidemodule">
