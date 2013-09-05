fcac15ec00c4a468fd8b2a67132b04841050d1d2
Chad Danford <chad.m.danford@gmail.com>
2013-09-05 17:34:26 +0200

    greybox

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <!-- sidebar -->
-			
+			<div class="greybox">
 	    				<figure>
 	    					<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
 	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
@@ -16,7 +16,7 @@
 					<figure>
 	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="">
 	    			</figure>
-	    			
+	    	</div>	
 
 <!-- /sidebar -->
 
