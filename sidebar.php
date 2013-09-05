2a1ca39feb5069dd3e490273be7f9b4ed3dbbd8e
Chad Danford <chad.m.danford@gmail.com>
2013-09-05 17:19:09 +0200

    sidebar changes

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,21 @@
 <!-- sidebar -->
-	    	<div class="sidemodule">
-	    		<div class="box">
+
 	    			<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
 	    				<figure>
 	    					<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
 	    				</figure>
 	    			</a>		
-	    		</div>
-	    	</div>
-	    	<div class="sidemodule">
-	    		<div class="box">
+
 	    			<a href="http://http://dah-bremerhaven.de/australien.php">
 	    				<figure>
 	    					<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="">
 	    				</figure>
 	    			</a>
-	    		</div>
-	    	</div>
-	    	<div class="sidemodule">
-	    		<div class="box">
-	    			<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="">
-	    		</div>
-	    	</div>
+
+					<figure>
+	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="">>
+	    			</figure>
+	    			
+
 <!-- /sidebar -->
 
