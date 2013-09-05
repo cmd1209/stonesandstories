125cd262b76de020a4c6df0e3dabbe05d680313f
Chad Danford <chad.m.danford@gmail.com>
2013-09-05 14:30:15 +0200

    brokenglass

--- header.php
+++ header.php
@@ -26,6 +26,9 @@
     </script>
   </head>
   <body <?php body_class(); ?>>
+  <div class="glass">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/brokenglass.png" alt="glass">
+  </div>
   
         <div class="masthead">
             <a href="<?php echo home_url(); ?>">
