0c2715c573ab76189898a670ad044927d35ce85f
Chad Danford <chad.m.danford@gmail.com>
2013-09-06 11:55:55 +0200

    exploring footer problems

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
     }()
     </script>
   </head>
-  <body <?php body_class(); ?>>
+  <body <?php body_class(); ?>
 <!-- <div class="glass">
     <img src="<?php echo get_template_directory_uri(); ?>/img/brokenglass.png" alt="glass">
   </div> -->
