494d30cd20027851dbfb2b74f5617f0f8ab0c15d
Chad Danford <chad.m.danford@gmail.com>
2013-09-09 14:01:38 +0200

    target = blank

--- sidebar.php
+++ sidebar.php
@@ -2,19 +2,19 @@
 			<div class="greybox">
 	    				<figure>
 	    					<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="">
+	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="" target="blank">
 	    					</a>
 	    				</figure>
 		
 
 	    				<figure>
 	    					<a href="http://www.dah-bremerhaven.de/australien.php">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="">
+	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" target="blank">
 	    					</a>
 	    				</figure>
 
 					<figure>
-	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="">
+	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="" target="blank">
 	    			</figure>
 	    	</div>
 <!-- /sidebar -->
