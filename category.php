9b377fb4e9299bcc41c059b599f560ca0977787b
Chad Danford <chad.m.danford@gmail.com>
2013-09-10 17:24:57 +0200

    image 100% wide

--- category.php
+++ category.php
@@ -7,10 +7,9 @@
 		<h1><?php single_cat_title(); ?></h1>
         <div class="col col100">
           <div class="module">
-            
 
-            		<div class="textwrap double categorylist">
-            		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<div class="textwrap double categorylist">
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
             		<li class="nobreak">
             		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
@@ -25,7 +24,7 @@
 					<?php endwhile; ?>
 					<?php endif; ?>
 					</li>
-            		</div>
+			</div>
 
           </div>
         </div>
