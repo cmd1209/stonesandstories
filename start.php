aa34fd683d7c673ff9e2fb4b5a2d31507c87f016
Chad Danford <chad.m.danford@gmail.com>
2013-09-11 16:01:16 +0200

    lets see again

--- start.php
+++ start.php
@@ -3,8 +3,9 @@
   <section role="main">
     <div class="col startcontent">
 
-      <div class="col col100 S">
-        <div class="module">
+      <div class="col col75 L">
+
+          <div class="module">
           <?php query_posts('orderby=rand&showposts=1&tag=t1'); ?>
         <?php while (have_posts()) : the_post(); ?>
             <h2><?php the_title(); ?></h2>
@@ -16,10 +17,9 @@
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
             <?php endwhile; ?>
         </div>
-      </div>
 
-      <div class="col col75 L">
-
+      
+      <div class="col col50 L">
         <div class="module">
           <?php query_posts('orderby=rand&showposts=1&tag=t2'); ?>
         <?php while (have_posts()) : the_post(); ?>
@@ -31,8 +31,10 @@
               <a href="<?php echo get_permalink(); ?>"> mehr...</a>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
             <?php endwhile; ?>
-        </div>
+        </div>        
+      </div>
 
+      <div class="col col50 R">
         <div class="module">
           <?php query_posts('orderby=rand&showposts=1&tag=t3'); ?>
         <?php while (have_posts()) : the_post(); ?>
@@ -44,7 +46,10 @@
               <a href="<?php echo get_permalink(); ?>"> mehr...</a>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
             <?php endwhile; ?>
-        </div>
+        </div>        
+      </div>
+
+
 
         <div class="module">
           <?php query_posts('orderby=rand&showposts=1&tag=t4'); ?>
@@ -62,7 +67,7 @@
       </div>
 
 
-      <div class="col col25">
+      <div class="col col25 R">
         <div class="module">
           <?php query_posts('orderby=rand&showposts=1&tag=t5'); ?>
         <?php while (have_posts()) : the_post(); ?>
