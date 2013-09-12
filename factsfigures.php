016ad4be3f30e77522ff11f678d41cee59d27219
Chad Danford <chad.m.danford@gmail.com>
2013-09-12 10:26:35 +0200

    rebuild

--- factsfigures.php
+++ factsfigures.php
@@ -5,7 +5,27 @@
     <div class="col maincontent">
       <div class="col col100">
         <div class="module">
-
+          <?php query_posts( array( 'category__in' => array(1,10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
+          <?php while (have_posts()) : the_post(); ?>
+          <div class="factbox">
+            <div class="col col25">
+              <a href="#"> <?php the_title(); ?></a>
+            </div>
+            <div class="col col10">
+              <?php the_date('Y', '<p>', '</p>'); ?>
+            </div>
+            <div class="col col25">
+              <?php the_category(); ?>
+            </div>
+            <div class="col col10">
+              <a href="<?php echo get_permalink(); ?>"> mehr...</a>
+            </div>
+            <div class="col col30">
+              
+            </div>     
+          </div>
+          <?php endwhile; ?>
+          <?php wp_reset_query(); ?>     
         </div>
       </div>
     </div>
