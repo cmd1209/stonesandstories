9792b86bd7756549fa1272e53515c3e6bf7dac1b
Chad Danford <chad.m.danford@gmail.com>
2013-09-12 15:38:46 +0200

    post categories

--- factsfigures.php
+++ factsfigures.php
@@ -21,7 +21,7 @@
               <?php the_tags('<ul class="post-categories"><li>','</li><li>','</li></ul>'); ?>
             </div>
             <div class="col accordion">
-              <a href="#"class="trigger">Details</a>
+              <a href="#"class="trigger post-categories">Details</a>
               <div class="toggle">
                <?php if( get_field('factsfigures') ): ?>
                     <?php the_field('factsfigures'); ?>
