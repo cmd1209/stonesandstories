49ac6ef6ba2a5027c3002923dec0e15f281d51b2
Chad Danford <chad.m.danford@gmail.com>
2013-09-12 10:06:31 +0200

    rearrange

--- factsfigures.php
+++ factsfigures.php
@@ -6,10 +6,10 @@
       
         <div class="col col100">
           <div class="module">
-            <div class="factbox">
-              <div class="col col25">
                 <?php query_posts( array( 'category__in' => array(1,10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
                 <?php while (have_posts()) : the_post(); ?>
+            <div class="factbox">
+              <div class="col col25">
                   <a href="#"> <?php the_title(); ?></a>
               </div>
               <div class="col col10">
