d8d1985e2d0efe9be2c66a79c88542d0c2712110
Chad Danford <chad.m.danford@gmail.com>
2013-09-12 10:30:45 +0200

    starting to look good

--- factsfigures.php
+++ factsfigures.php
@@ -8,19 +8,19 @@
           <?php query_posts( array( 'category__in' => array(1,10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
-            <div class="col col25">
+            <div class="col col40">
               <a href="#"> <?php the_title(); ?></a>
             </div>
             <div class="col col10">
               <?php the_date('Y', '<p>', '</p>'); ?>
             </div>
-            <div class="col col25">
+            <div class="col col20">
               <?php the_category(); ?>
             </div>
             <div class="col col10">
               <a href="<?php echo get_permalink(); ?>"> mehr...</a>
             </div>
-            <div class="col col30">
+            <div class="col col20">
               
             </div>     
           </div>
