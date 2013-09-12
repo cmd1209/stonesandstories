406601def8773f91e8a415c8edc5f463fb270f87
Chad Danford <chad.m.danford@gmail.com>
2013-09-12 10:18:16 +0200

    wow

--- factsfigures.php
+++ factsfigures.php
@@ -3,6 +3,10 @@
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main">
     <div class="col maincontent">
+
+
+    <div class="col col100">
+      
       <div class="module">
         <?php query_posts( array( 'category__in' => array(1,10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
         <?php while (have_posts()) : the_post(); ?>
@@ -26,9 +30,10 @@
 
               <?php wp_reset_query(); ?>           
 
+      </div>
 
       </div>
-    </div>
+
     <div class="col sidebar">
       <?php get_sidebar(); ?>
     </div>
