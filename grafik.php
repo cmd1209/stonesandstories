a8b3d1b73e5f507a0e5b3f0aa75017a5f142ddf9
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:34:24 +0200

    test

--- grafik.php
+++ grafik.php
@@ -1,15 +1,14 @@
 <?php /* Template Name: grafik */ get_template_part('grafikheader'); ?>
 
 <div class="grafik">
+  <div class="col col25">
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
-  <div class="col col25">
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
             <?php endwhile; ?>
     
   </div>
 </div>
-
 
 <?php get_template_part('grafikfooter'); ?>
 
