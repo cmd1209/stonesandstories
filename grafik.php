0366647f843b11a4b2fb02ca892487fa26cef0d8
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:07:55 +0200

    module break

--- grafik.php
+++ grafik.php
@@ -2,7 +2,7 @@
 
   <section role="main">
     <div class="col100">
-          <div class="module">
+          <div class="grafik">
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
