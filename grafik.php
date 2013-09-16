50f6a451f277f31dea19b42cb293bf141cde2944
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:55:32 +0200

    grafik complete neu

--- grafik.php
+++ grafik.php
@@ -1,20 +1,30 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
-<section role="main" >
-  <div class="col col100">
-    <div class="col col25">
+  <section role="main">
+    <div class="col col100">
 
+      <div class="col col25">
+          <div class="module">
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-
+                  <div class="textwrap">
+                  <h2><?php the_title(); ?></h2>
+                  <?php if( get_field('subheadline') ): ?>
+                    <h3><?php the_field('subheadline'); ?></h3>
+                  <?php endif; ?>
+                  <?php echo excerpt(20); ?>
+                  <a href="<?php echo get_permalink(); ?>"> mehr...</a>
+                </div>
             <?php endwhile; ?>
-
+          </div>
         </div>
-  </div>
-</section>
+      </div>
+
+
+
+    </div>
+  </section>
 
 <?php get_footer(); ?>
-
-
 
