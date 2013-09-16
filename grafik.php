9e67288d6ff2e67792e28cbf2d1cc4de64243043
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:57:45 +0200

    float left gone

--- grafik.php
+++ grafik.php
@@ -1,10 +1,11 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
   <section role="main">
-    <div class="col col100">
+    <div class="col100">
 
       <div class="col col25 H400">
           <div class="module">
+
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
