6ee1deb8c75aae220e009d486fb80df7b9b23a4e
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:49:08 +0200

    grafik new

--- grafik.php
+++ grafik.php
@@ -2,14 +2,15 @@
 
 <section role="main" >
   <div class="col col100">
-    <?php query_posts('orderby=rand&cat=12'); ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <div class="col col25">
-        <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medum->ID, 'medium');?></a>
-      </div>
-    <?php endwhile; ?>
-    <?php wp_reset_query(); ?>     
-      
+    <div class="col col25">
+
+            <?php query_posts('orderby=rand&cat=12'); ?>
+              <?php while (have_posts()) : the_post(); ?>
+              <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
+
+            <?php endwhile; ?>
+
+        </div>
   </div>
 </section>
 
