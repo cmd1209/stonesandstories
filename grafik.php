83852a1e90af8c558e2f969a49c9483cb7fbf2a6
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:38:34 +0200

    try2

--- grafik.php
+++ grafik.php
@@ -2,12 +2,11 @@
 
 <div class="grafik">
             <?php query_posts('orderby=rand&cat=12'); ?>
-  <div class="col col25">
               <?php while (have_posts()) : the_post(); ?>
-              <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-            <?php endwhile; ?>
-    
+  <div class="col col25">
+    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
   </div>
+            <?php endwhile; ?>
 </div>
 
 <?php get_template_part('grafikfooter'); ?>
