27a1e0249e2a823194b83c52beaacf8a0a3ef0f1
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:06:03 +0200

    almost giving up

--- grafik.php
+++ grafik.php
@@ -2,18 +2,12 @@
 
   <section role="main">
     <div class="col100">
-
-      <div class="col col25 H400">
           <div class="module">
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
             <?php endwhile; ?>
           </div>
-        </div>
-
-
-
     </div>
   </section>
 
