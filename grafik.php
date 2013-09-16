37ae755eb14521185fbb1e946d3a5fd9394570df
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:29:41 +0200

    amazing

--- grafik.php
+++ grafik.php
@@ -1,16 +1,16 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <section role="main" >
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <div class="module">
-    <div class="col col25">
       <?php query_posts('orderby=rand&cat=12'); ?>
+    <div class="col col25">
       <?php while (have_posts()) : the_post(); ?>
           <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medum->ID, 'medium');?></a>
+    </div>
+    </div>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>     
-    </div>
-    </div>
       
     </section>
   <?php endwhile; ?>
