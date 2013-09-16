37a474b52b2ee241ef412b67f647d26e9a4e33ff
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 16:54:14 +0200

    lots of fun

--- grafik.php
+++ grafik.php
@@ -5,3 +5,14 @@
 </div>
 
 <?php get_footer(); ?>
+<div id="overlay" style="display: block;">
+<div class="gprojects">
+   <?php query_posts('cat=12'); ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="col col25">
+          <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
+  </div>
+  <?php endwhile; endif; ?>
+
+</div>
+</div>
