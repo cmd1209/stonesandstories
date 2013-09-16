8f137af6ed61f84ba874510747fe3748ba073a3d
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 10:11:35 +0200

    grafik changes

--- grafik.php
+++ grafik.php
@@ -1,22 +1,16 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-  <section role="main">
-    <div class="col grafik">
-      <div class="col col100">
-        <div class="module">
+    <div class="grafik">
           <?php query_posts( array( 'category__in' => array(11), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-          <div class="col col50">
+          <div class="col col25">
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
               <a href="#" class="trigger"><?php the_title(); ?></a>
           </div>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>     
-        </div>
-        </div>
       </div>
-  </section>
   <?php endwhile; ?>
   <?php endif; ?>
 <?php get_footer(); ?>
