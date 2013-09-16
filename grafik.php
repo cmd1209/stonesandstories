dde79cbbd617421cb4bb1b2f3419f1821a68d81e
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 16:47:58 +0200

    lets have some fun now

--- grafik.php
+++ grafik.php
@@ -1,14 +1,7 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
 <div class="grafik">
-  <?php query_posts('cat=12'); ?>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <div class="col col25">
-    <div class="biglinks H500">
-          <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
-    </div>
-  </div>
-  <?php endwhile; endif; ?>
+ 
 </div>
 
 <?php get_footer(); ?>
