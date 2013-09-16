509a23c01e67e6683a4ca26f22c472781c65a272
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 09:59:54 +0200

    grafik changes

--- grafik.php
+++ grafik.php
@@ -2,7 +2,7 @@
 
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main">
-    <div class="col maincontent">
+    <div class="col grafik">
       <div class="col col100">
         <div class="module">
           <?php query_posts( array( 'category__in' => array(11), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
@@ -10,16 +10,12 @@
           <div class="col col50">
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
               <a href="#" class="trigger"><?php the_title(); ?></a>
+          </div>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>     
-          </div>
         </div>
         </div>
       </div>
-
-    <div class="col sidebar">
-      <?php get_sidebar(); ?>
-    </div>
   </section>
   <?php endwhile; ?>
   <?php endif; ?>
