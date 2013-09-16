ceb9d2974e9ef37caca00a619e8d7febb7ee4a8f
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:36:24 +0200

    i think we have it

--- grafik.php
+++ grafik.php
@@ -1,8 +1,8 @@
 <?php /* Template Name: grafik */ get_template_part('grafikheader'); ?>
 
 <div class="grafik">
-  <div class="col col25">
             <?php query_posts('orderby=rand&cat=12'); ?>
+  <div class="col col25">
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
             <?php endwhile; ?>
