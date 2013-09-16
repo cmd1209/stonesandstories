5e5b6c0eb31a2dce56999d71cf5ec5691ed5b129
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 12:01:47 +0200

    columsn

--- grafik.php
+++ grafik.php
@@ -5,22 +5,12 @@
 
       <div class="col col25 H400">
           <div class="module">
-
             <?php query_posts('orderby=rand&cat=12'); ?>
               <?php while (have_posts()) : the_post(); ?>
               <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-                  <div class="textwrap">
-                  <h2><?php the_title(); ?></h2>
-                  <?php if( get_field('subheadline') ): ?>
-                    <h3><?php the_field('subheadline'); ?></h3>
-                  <?php endif; ?>
-                  <?php echo excerpt(20); ?>
-                  <a href="<?php echo get_permalink(); ?>"> mehr...</a>
-                </div>
             <?php endwhile; ?>
           </div>
         </div>
-      </div>
 
 
 
