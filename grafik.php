9cf0aeda1991468830b57645287f9f355f11ae2e
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:37:19 +0200

    getting freaky

--- grafik.php
+++ grafik.php
@@ -1,18 +1,17 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
-
-    <section role="main" >
-    <div class="col col100">
-      <?php query_posts('orderby=rand&cat=12'); ?>
-    <div class="col col25">
-      <?php while (have_posts()) : the_post(); ?>
-          <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medum->ID, 'medium');?></a>
-    </div>
-    </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>     
+<section role="main" >
+  <div class="col col100">
+    <?php query_posts('orderby=rand&cat=12'); ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <div class="col col25">
+        <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medum->ID, 'medium');?></a>
+      </div>
+    <?php endwhile; ?>
+    <?php wp_reset_query(); ?>     
       
-    </section>
+  </div>
+</section>
 
 <?php get_footer(); ?>
 
