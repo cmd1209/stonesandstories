2d09d5bd057f54f8b892b905e9939e989bd4bbbb
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:06:58 +0200

    grafik part2

--- grafik.php
+++ grafik.php
@@ -6,8 +6,7 @@
           <?php query_posts( array( 'category__in' => array(11), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
           <div class="col col25">
-              <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
-              <a href="#" class="trigger"><?php the_title(); ?></a>
+              <div class="imagecontainer"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a></div>
           </div>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>     
