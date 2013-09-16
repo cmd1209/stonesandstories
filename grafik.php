5fa8aaf58d5c3a7d6ef0910b0933d98ad4e61911
Chad Danford <chad.m.danford@gmail.com>
2013-09-16 11:25:20 +0200

    dont know

--- grafik.php
+++ grafik.php
@@ -3,11 +3,11 @@
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <section role="main" >
     <div class="grafik">
-          <li class="nobreak">
-           <?php query_posts('orderby=rand&cat=12'); ?>
-          <?php while (have_posts()) : the_post(); ?>
-              <div class="imagecontainer"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'medium');?></a></div>
-          </li>
+      <?php query_posts('orderby=rand&cat=12'); ?>
+        <li class="nobreak">
+      <?php while (have_posts()) : the_post(); ?>
+          <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medum->ID, 'medium');?></a>
+        </li>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>     
     </div>
