6d5005f6ae92156fd8616da79baa575c803dd764
Chad Danford <chad.m.danford@gmail.com>
2013-09-17 16:26:39 +0200

    video

--- video.php
+++ video.php
@@ -2,17 +2,12 @@
 
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main">
-    <div class="col maincontent">
-      <div class="col col100">
-        <div class="module videoatmo">
+    <div class="videoatmo">
+
 
         <?php the_content(); ?>
         
-        </div>
-      </div>
-    </div>
-    <div class="col sidebar">
-      <?php get_sidebar(); ?>
+
     </div>
   </section>
     <?php endwhile; ?>
