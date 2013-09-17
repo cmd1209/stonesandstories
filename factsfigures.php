ceaae2cbb9ac70c1515aaf9d7ec52e013e22e34a
Chad Danford <chad.m.danford@gmail.com>
2013-09-17 12:24:17 +0200

    php the category

--- factsfigures.php
+++ factsfigures.php
@@ -4,41 +4,10 @@
   <section role="main">
     <div class="col maincontent">
 
-      <div class="col col100">
-        <div class="module">
-          <?php query_posts( array( 'category__in' => array(2), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
-            <h2><?php single_cat_title(); ?></h2>
-          <?php while (have_posts()) : the_post(); ?>
-          <div class="factbox">
-            <div class="col col50" style="padding-right: 20px;">
-              <a href="#" class="trigger"><?php the_title(); ?></a>
-            </div>
-            <div class="col col10">
-              <?php the_date('Y', '<p>', '</p>'); ?>
-            </div>
-            <div class="col col10" style="min-height: 1px;">
-               <?php if( get_field('location') ): ?>
-                    <?php the_field('location'); ?>
-                <?php endif; ?>
-            </div>
-            <div class="col col30">
-              <?php the_tags('<ul class="post-categories"><li>','</li><li>','</li></ul>'); ?>
-            </div>
-              <div class="toggle">
-               <?php if( get_field('factsfigures') ): ?>
-                    <?php the_field('factsfigures'); ?>
-                <?php endif; ?>
-              </div>     
-          </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>     
-        </div>
-        </div>
-        
         <div class="col col100">
         <div class="module">
-          <?php query_posts( array( 'category__in' => array(10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
-            <h2><?php single_cat_title(); ?></h2>
+          <?php query_posts( array( 'category__in' => array(60), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
+            <h2><?php the_category(); ?> </h2>
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
@@ -68,8 +37,8 @@
 
     <div class="col col100">
     <div class="module">
-          <?php query_posts( array( 'category__in' => array(60), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
-            <h2><?php single_cat_title(); ?></h2>
+          <?php query_posts( array( 'category__in' => array(10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
+            <h2><?php the_category(); ?> </h2>
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
