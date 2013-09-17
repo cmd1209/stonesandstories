16da43ac50e402053cf228d343b240a866b2f8d3
Chad Danford <chad.m.danford@gmail.com>
2013-09-17 12:10:46 +0200

    order change

--- factsfigures.php
+++ factsfigures.php
@@ -5,9 +5,9 @@
     <div class="col maincontent">
       <div class="col col100">
         <div class="module">
+            <h2><?php single_cat_title(); ?></h2>
           <?php query_posts( array( 'category__in' => array(2), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-            <h2><?php single_cat_title(); ?></h2>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
               <a href="#" class="trigger"><?php the_title(); ?></a>
@@ -34,9 +34,9 @@
         </div>
 
         <div class="module">
+            <h2><?php single_cat_title(); ?></h2>
           <?php query_posts( array( 'category__in' => array(10), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-            <h2><?php single_cat_title(); ?></h2>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
               <a href="#" class="trigger"><?php the_title(); ?></a>
@@ -66,9 +66,9 @@
 
 
     <div class="module">
+            <h2><?php single_cat_title(); ?></h2>
           <?php query_posts( array( 'category__in' => array(60), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-            <h2><?php single_cat_title(); ?></h2>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
               <a href="#" class="trigger"><?php the_title(); ?></a>
