8086b557069bbca35abe14f598c1b191a25cd238
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 12:13:14 +0200

    inline changes to grafik style

--- category-grafik.php
+++ category-grafik.php
@@ -4,18 +4,15 @@
 	<section role="main">
 
         <div class="col col100">
-          <div class="module" style="background-color:black; color: white;">
+          <div class="module" style="background-color:black;">
 
 			<div class="textwrap double categorylist">
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
             		<li class="nobreak">
-            		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+            		<h2 style="color:white;"> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 					<?php endif; ?>
-					<?php echo excerpt(35); ?>
-					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?>  
 					<?php endwhile; ?>
 					<?php endif; ?>
 					</li>
