c8e920028ea7f6bc9da3f5733b93577ffb80bd31
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:58:16 +0200

    grafik hanges

--- grafik.php
+++ grafik.php
@@ -1,6 +1,5 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main" class="black">
   <div class="col col100">
       <?php query_posts( array( 'category__in' => array(12), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
@@ -8,12 +7,11 @@
     <div class="col col50">
       <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
     </div>
+  </div>
+  </section>
       <?php endwhile; ?>
       <?php wp_reset_query(); ?>
-    </div>
-  </section>
-    <?php endwhile; ?>
-  <?php endif; ?>
+
 <?php get_footer(); ?>
 
 
