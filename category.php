55d1915944420b66b00a7ad22fde17d4bb03c23b
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 13:54:11 +0200

    test

--- category.php
+++ category.php
@@ -12,7 +12,7 @@
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
             		<li class="nobreak">
             		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 	<?php if (class_exists('MultiPostThumbnails')) : ?>
 		<?php  MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image'); endif; ?>
@@ -20,7 +20,7 @@
 		<?php the_post_thumbnail('large'); ?>
 <?php endif; ?>
 						</a>
-					<?php endif; ?>
+
 					<?php echo excerpt(35); ?>
 					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
 					<?php the_category(); ?>  
