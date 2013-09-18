f80bc113fe504f2f95999bc571378882e7de74b3
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 15:52:43 +0200

    no break gone

--- category-grafik.php
+++ category-grafik.php
@@ -8,7 +8,7 @@
 
 			<div class="gdouble">
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-            		<li class="nobreak">
+            		<li>
             		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
