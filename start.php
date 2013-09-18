768f2d38963ca1780061d658d093744422022cb6
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 09:57:58 +0200

    ok lets see

--- start.php
+++ start.php
@@ -4,8 +4,8 @@
 
       <div class="row clear">
         <div class="col col75 L">
-
-<div class="module">          <?php $ids = array(73, 65); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
+          <div class="box">
+          <?php $ids = array(73, 65); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
             <?php while (have_posts()) : the_post(); ?>
               <h2><?php the_title(); ?></h2>
                 <?php if( get_field('subheadline') ): ?>
@@ -14,12 +14,13 @@
               <?php echo excerpt(60); ?>
             <a href="<?php echo get_permalink(); ?>"> mehr...</a>
           <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
-          <?php endwhile; ?> </div>
-
+          <?php endwhile; ?>
+          </div>
         </div>
-        <div class="col col25 R">
 
-          <?php $ids = array(506,421); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
+        <div class="col col25 R">
+          <div class="box">
+            <?php $ids = array(506,421); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
             <?php while (have_posts()) : the_post(); ?>
               <h2><?php the_title(); ?></h2>
                 <?php if( get_field('subheadline') ): ?>
@@ -28,20 +29,23 @@
               <?php echo excerpt(60); ?>
             <a href="<?php echo get_permalink(); ?>"> mehr...</a>
           <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
-          <?php endwhile; ?> 
-          
+          <?php endwhile; ?>           
+          </div>
         </div>
       </div> <!--  end row -->
 
       <div class="row clear">
         <div class="col col50 L">
+        <div class="box">
           <?php $ids = array(380); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
             <?php while (have_posts()) : the_post(); ?>
             <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
           <?php endwhile; ?> 
         </div>
+        </div>
 
         <div class="col col50 R">
+        <div class="box">
           <?php $ids = array(619, 592); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
               <?php while (have_posts()) : the_post(); ?>
                   <h2 style="margin-top:5px;"><?php the_title(); ?></h2>
@@ -51,6 +55,7 @@
                   <?php echo excerpt(15); ?>
                   <a href="<?php echo get_permalink(); ?>"> mehr...</a>
             <?php endwhile; ?>
+          </div>
         </div>
       </div>  <!-- end row -->
 
