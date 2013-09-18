f621fbf07c69177317610e6c6777937fb609625b
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:43:52 +0200

    grafik posts

--- grafik.php
+++ grafik.php
@@ -3,12 +3,12 @@
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main" class="black">
   <div class="col col100">
-      <div class="col col50">
         <?php query_posts('cat=12'); ?>  
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="col col50">
           <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-        <?php endwhile; endif; ?>
       </div>
+        <?php endwhile; endif; ?>
     </div>
   </section>
     <?php endwhile; ?>
