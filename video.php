d390cbe40989532cd7d30a007ccc2c2d59b7fe38
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:24:36 +0200

    background black

--- video.php
+++ video.php
@@ -1,15 +1,13 @@
 <?php /* Template Name: video */ get_header(); ?>
 
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-  <section role="main">
-    <div class="videoatmo">
+  <section role="main" class="black">
       <div class="col col75 L">
         <?php the_content(); ?>
       </div>
       <div class="col col25 R">
         
       </div>
-    </div>
   </section>
     <?php endwhile; ?>
   <?php endif; ?>
