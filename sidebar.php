b85da131293bf68096ff05ef2a02f404ee3dd81c
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 12:00:13 +0200

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,22 @@
 <!-- sidebar -->
 			<div class="greybox">
 	    				<figure>
-	    					<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="" target="blank">
+	    					<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/" target="blank">
+	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/humbold-k.jpg" alt="" >
 	    					</a>
 	    				</figure>
 		
 
 	    				<figure>
-	    					<a href="http://www.dah-bremerhaven.de/australien.php">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" target="blank">
+	    					<a href="http://www.dah-bremerhaven.de/australien.php" target="blank">
+	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" >
 	    					</a>
 	    				</figure>
 
-					<figure>
-	    					<img src="http://andreas-heller.de/2013/wp-content/uploads/2013/06/green.jpg" alt="" target="blank">
-	    			</figure>
+						<figure>
+							<a href="http://www.iba-hamburg.de/" target="blank">
+	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/News_Talks-and-Events_IBA-Logo.jpg" alt="" >
+	    					</a>
+	    				</figure>
 	    	</div>
 <!-- /sidebar -->
-
