0d1bc1e52f48619dec116bbb0cae888fb7281b83
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 16:47:54 +0200

    block

--- category-grafik.php
+++ category-grafik.php
@@ -7,9 +7,9 @@
           <div class="module" style="background-color:black;">
 
 			<div class="gdouble nobreak">
-			<div class="block">
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+            			<div class="block">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 						<?php
 if (class_exists('MultiPostThumbnails')) :
