944b6b2ee01938ee62699438f1836a6ff65120e3
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:39:13 +0200

    grafik changes

--- grafik.php
+++ grafik.php
@@ -3,11 +3,11 @@
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main" class="black">
   <div class="col col100">
-    <?php query_posts('cat=12'); ?>  
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <div class="col col50">
-        <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-          <?php endwhile; endif; ?>
+        <?php query_posts('cat=12'); ?>  
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
+        <?php endwhile; endif; ?>
       </div>
     </div>
   </section>
