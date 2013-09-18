cc77ff1592511b54e547c1d09a478bf35f1cd818
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 12:35:52 +0200

    category test

--- category.php
+++ category.php
@@ -12,6 +12,11 @@
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
             		<li class="nobreak">
             		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+            			<?php if (is_category('ausstellung')) : ?>
+<p>hey cool ausstellung</p>
+<?php else : ?>
+<p>no nothing to report</p>
+<?php endif; ?>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 					<?php endif; ?>
