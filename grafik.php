95ef62c7f894f5db4514a95d1c34a5b8b69f6ae6
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:59:21 +0200

    cant get it right

--- grafik.php
+++ grafik.php
@@ -1,17 +1,19 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main" class="black">
   <div class="col col100">
       <?php query_posts( array( 'category__in' => array(12), 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC' ) ); ?>
-      <?php while (have_posts()) : the_post(); ?>
     <div class="col col50">
+      <?php while (have_posts()) : the_post(); ?>
       <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
     </div>
-  </div>
-  </section>
       <?php endwhile; ?>
       <?php wp_reset_query(); ?>
-
+  </div>
+  </section>
+    <?php endwhile; ?>
+  <?php endif; ?>
 <?php get_footer(); ?>
 
 
