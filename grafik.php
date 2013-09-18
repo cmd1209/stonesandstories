b05343883912b4652a10fe7f2773758b6e0a2348
Chad Danford <chad.m.danford@gmail.com>
2013-09-18 10:33:18 +0200

    grafik changes

--- grafik.php
+++ grafik.php
@@ -1,18 +1,23 @@
 <?php /* Template Name: grafik */ get_header(); ?>
 
-<div class="grafik">
- 
-</div>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+  <section role="main" class="black">
+    <?php the_content(); ?>
 
+    <?php query_posts('cat=12'); ?>  
+      <div class="col col50">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <a class="grafiklinks col" href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
+          <?php endwhile; endif; ?>
+      </div>
+  </section>
+    <?php endwhile; ?>
+  <?php endif; ?>
 <?php get_footer(); ?>
 
-<div class="gprojects">
-   <?php query_posts('cat=12'); ?>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-          <a class="grafiklinks col" href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($medium->ID, 'medium');?></a>
-
-  <?php endwhile; endif; ?>
 
 
-</div>
+  
+
+          
+
