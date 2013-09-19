2ec0a2f9c6bbd1fcbfdb4173898a711932bcdca9
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 12:19:38 +0200

    lets see

--- category-hot.php
+++ category-hot.php
@@ -8,44 +8,19 @@
           <div class="module">
 		<h1><?php single_cat_title(); ?></h1>
 
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+<div class="col col50">
+<h1><?php the_permalink(); ?></h1>
+<?php the_content(); ?>
 
-<div class="col col50 L">
-<div class="textwrap">
-<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-					<?php endif; ?>
-					<?php echo excerpt(35); ?>
-					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?> 
-</div>
+
 </div>
 
-<?php endif; endwhile; else: ?>
-
+<?php endwhile; else: ?>
 <?php endif; ?>
 
-<?php $i = 0; rewind_posts(); ?>
 
-<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-
-<div class="col col50 R">
-<div class="textwrap">
-<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-					<?php endif; ?>
-					<?php echo excerpt(35); ?>
-					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?> 
-</div>
-</div>
-
-<?php endif; endwhile; else: ?>
-
-<?php endif; ?>
 
 
 
@@ -62,3 +37,8 @@
 
 
 <?php get_footer(); ?>
+
+
+
+
+
