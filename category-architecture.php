51fb872e4b68f5fae3f8ab430e1e86e9968280da
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 15:58:04 +0200

    subheadlines

--- category-architecture.php
+++ category-architecture.php
@@ -7,12 +7,15 @@
 		<div class="col col100">
 			<div class="module">
 				<h1><?php single_cat_title(); ?></h1>
-				<div class="col col50 L">
+				<div class="col col50">
 
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
 						<div class="textwrap underline">
 							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+							<?php if( get_field('subheadline') ): ?>
+                    			<h3><?php the_field('subheadline'); ?></h3>
+                 			 <?php endif; ?>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 							<?php echo excerpt(35); ?>
 							<a href="<?php echo get_permalink(); ?>"> mehr...</a>
@@ -24,11 +27,14 @@
 				<?php endif; ?>
 			</div>
 			<?php $i = 0; rewind_posts(); ?>
-			<div class="col col50 R">
+			<div class="col col50">
 				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-					
+
 					<div class="underline textwrap">
 						<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<?php if( get_field('subheadline') ): ?>
+                    		<h3><?php the_field('subheadline'); ?></h3>
+                  		<?php endif; ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 						<?php echo excerpt(35); ?>
 						<a href="<?php echo get_permalink(); ?>"> mehr...</a>
