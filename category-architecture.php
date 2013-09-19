75a965141844396d1e77c40f1114f3ced121a599
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 17:18:06 +0200

    margins out

--- category-architecture.php
+++ category-architecture.php
@@ -12,10 +12,10 @@
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
 						<div class="textwrap underline">
-							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?> | 							<?php if( get_field('subheadline') ): ?>
-                    			<span style="font-size:1em;"><?php the_field('subheadline'); ?></span>
-                 			 <?php endif; ?></a></h2>
-
+							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+							<?php if( get_field('subheadline') ): ?>
+                    			<h3><?php the_field('subheadline'); ?></h3>
+                 			 <?php endif; ?>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 							<?php echo excerpt(35); ?>
 							<a href="<?php echo get_permalink(); ?>"> mehr...</a>
