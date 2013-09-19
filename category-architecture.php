ab00b3121a3c8f149fbcad29766777fa36fa6f8d
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 14:20:14 +0200

    back to normal

--- category-architecture.php
+++ category-architecture.php
@@ -8,21 +8,20 @@
           <div class="module">
 			<h1><?php single_cat_title(); ?></h1>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-<div class="textwrap col col50 underline" style="height: 600px;">
-<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+			<div class="textwrap double categorylist">
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+            		<li class="nobreak">
+            		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 					<?php endif; ?>
 					<?php echo excerpt(35); ?>
 					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?>
-
-</div>
-
-<?php endwhile; else: ?>
-<?php endif; ?>
+					<?php the_category(); ?>  
+					<?php endwhile; ?>
+					<?php endif; ?>
+					</li>
+			</div>
 
           </div>
         </div>
