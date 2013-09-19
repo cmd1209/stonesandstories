9ab108e957deb8e40d066fca933204f2a5f8d2c4
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 15:46:50 +0200

    test this

--- category-architecture.php
+++ category-architecture.php
@@ -7,10 +7,11 @@
         <div class="col col100">
           <div class="module">
 			<h1><?php single_cat_title(); ?></h1>
+			<div class="col col50 L">
 
 			<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
-				<div class="col col50 textwrap underline">
+				<div class="textwrap underline">
 					<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 					<?php echo excerpt(35); ?>
@@ -22,12 +23,12 @@
 			<?php endif; endwhile; else: ?>
 			<div>Alternate content</div>
 		<?php endif; ?>
-
+		</div>
 		<?php $i = 0; rewind_posts(); ?>
-
+		<div class="col col50 R">
 		<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-
-			<div class="col col50 underline textwrap">
+			
+			<div class="underline textwrap">
 				<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 				<?php echo excerpt(35); ?>
@@ -39,7 +40,7 @@
 		<?php endif; endwhile; else: ?>
 		<div>Alternate content</div>
 	<?php endif; ?>
-
+	</div>
           </div>
         </div>
 
