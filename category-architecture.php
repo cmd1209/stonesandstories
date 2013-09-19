d7231e6ce2911b24073d1e501b0c557a345502b2
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 17:24:37 +0200

    fun

--- category-architecture.php
+++ category-architecture.php
@@ -6,7 +6,7 @@
 	<div class="col maincontent">
 		<div class="col col100">
 			<div class="module">
-				<h1><?php single_cat_title(); ?><p><?php echo category_description(); ?></p></h1>
+				<h1><?php single_cat_title(); ?></h1><p><?php echo category_description(); ?></p>
 				<div class="col col50">
 
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
