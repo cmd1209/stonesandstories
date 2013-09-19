4c6f67472d8e889847c8b8efe02726aadfe1ff50
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 09:55:30 +0200

    sticky top

--- js/scripts.js
+++ js/scripts.js
@@ -42,12 +42,12 @@ $(function(){
                         $('.navigation').css({position: 'fixed', top: '0px'});
                         stickylogo.slideDown(200);
                         searchform.css({top: '7px', right: '7px'});
-                        stage.css({"padding-top":"200px"});
+                        stage.css({"padding-top":"stickynavtop"});
                 } else {
                         $('.navigation').css({position: 'static', top: '0px'});
                         stickylogo.css({display: 'none'});
                         searchform.css({top: '-35px', right: '0px'});
-                        stage.css({"padding-top":"0px"});
+                        stage.css({"padding-top":"stickynavtop"});
                 }
         });
 });
