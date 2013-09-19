093e776767d28b60b508333777377789b4457caa
Chad Danford <chad.m.danford@gmail.com>
2013-09-19 12:07:14 +0200

    hot again

--- category-hot.php
+++ category-hot.php
@@ -8,59 +8,45 @@
           <div class="module">
 		<h1><?php single_cat_title(); ?></h1>
 
-			<?php if (have_posts()) : ?>
 
-    <?php while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
-        <div class="col col50 L">
-        <div class="textwrap">
-        <?php
-            $count++;
-
-            if( $count % 2 ) {
-            }
-            else {
-        ?>
-                <h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+<div class="col col50 L">
+<div class="textwrap">
+<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
 					<?php endif; ?>
 					<?php echo excerpt(35); ?>
 					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
 					<?php the_category(); ?> 
-        <?php
-            }
-        ?>
-        </div>
-        </div>
+</div>
+</div>
 
-        <div class="col col50 R">
-        	<div class="textwrap">
-        <?php
-            $count++;
-            if( $count % 2 ) {
-        ?>
-                <h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-					<?php endif; ?>
-					<?php echo excerpt(35); ?>
-					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?> 
-        <?php
-            }
-        ?>
-        </div>
-        </div>
-
-    <?php endwhile; ?>
-
-<?php else : ?>
+<?php endif; endwhile; else: ?>
 
 <?php endif; ?>
 
+<?php $i = 0; rewind_posts(); ?>
 
-				
+<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
+
+<div class="col col50 R">
+<div class="textwrap">
+<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
+					<?php endif; ?>
+					<?php echo excerpt(35); ?>
+					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
+					<?php the_category(); ?> 
+</div>
+</div>
+
+<?php endif; endwhile; else: ?>
+
+<?php endif; ?>
+
 
 
           </div>
@@ -76,16 +62,3 @@
 
 
 <?php get_footer(); ?>
-
-
-
-
-
-	
-		<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-					<?php endif; ?>
-					<?php echo excerpt(35); ?>
-					<a href="<?php echo get_permalink(); ?>"> mehr...</a>
-					<?php the_category(); ?> 
