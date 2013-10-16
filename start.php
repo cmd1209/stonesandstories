bf51d3ea93340c5810312502a5b72e23510bc37b
Chad Danford <chad.m.danford@gmail.com>
2013-10-16 12:58:00 +0200

    no idea

--- start.php
+++ start.php
@@ -4,7 +4,7 @@
     <div class="col startcontent">
 
 
-        <div class="col col100 H500">
+        <div class="col col100 H500 S">
           <div class="module">
             <div class="col col40">
              <?php $ids = array(73,69,65,102); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
@@ -59,7 +59,7 @@
         </div>
         
         <div class="col col50 R">
-        <div class="col col50 L H500">
+        <div class="col col100 S H500">
           <div class="module">
             <?php $ids = array(592,631); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
               <?php while (have_posts()) : the_post(); ?>
@@ -76,24 +76,7 @@
           </div>
         </div>
 
-        <div class="col col50 R H500">
-          <div class="module">
-            <?php $ids = array(219,421,519,629); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
-              <?php while (have_posts()) : the_post(); ?>
-              <div class="imagecontainer H500">
-                <a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a>
-              </div>
-                  <div class="textwrap yellow col100">
-                  <h2 style="margin-top:5px;"><?php the_title(); ?></h2>
-                  <?php if( get_field('subheadline') ): ?>
-                    <h3><?php the_field('subheadline'); ?></h3>
-                  <?php endif; ?>
-                  <?php echo excerpt(20); ?>
-                  <a href="<?php echo get_permalink(); ?>"> mehr...</a>
-                </div>
-            <?php endwhile; ?>
-          </div>
-        </div>
+  
         </div>
 
     </div>
