e928a2211e28d14539dc3bf1b08d2d7a2fcf37d3
Chad Danford <chad.m.danford@gmail.com>
2013-10-17 16:36:28 +0200

    releative post categories

--- single.php
+++ single.php
@@ -6,12 +6,12 @@
         <div class="col col100">
           <div class="module">
             <div class="textwrap categorysingle">
-                <?php the_category(); ?>              
                 <h2><?php the_title(); ?></h2>
                 <?php if( get_field('subheadline') ): ?>
                     <h3><?php the_field('subheadline'); ?></h3>
                   <?php endif; ?>
               <?php the_content(); ?>
+                <?php the_category(); ?>              
             </div>
           </div>
         </div>
