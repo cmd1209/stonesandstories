0243864c9fb7cceed25e013652ae4f39ab3f4904
Chad Danford <chad.m.danford@gmail.com>
2013-10-18 12:25:59 +0200

    lets see if tag works

--- tag.php
+++ tag.php
@@ -1,16 +1,80 @@
 <?php get_header(); ?>
 	
-	<!-- section -->
-	<section role="main">
-	
-		<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
-	
-		<?php get_template_part('loop'); ?>
-		
-		<?php get_template_part('pagination'); ?>
-	
-	</section>
-	<!-- /section -->
+<!-- section -->
+<section role="main">
+
+	<div class="col maincontent">
+		<div class="col col100">
+			<div class="module">
+				<h1><span><?php single_cat_title(); ?></span> Projekte</h1>
+				<div class="col col50">
+
+					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+
+						<div class="textwrap underline">
+							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+							<?php if( get_field('subheadline') ): ?>
+								<h3><?php the_field('subheadline'); ?></h3>
+							<?php endif; ?>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php
+								if (class_exists('MultiPostThumbnails')) :
+									if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
+										MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
+									else : ?>
+									<?php the_post_thumbnail('large'); ?>
+									<?php
+									endif;
+									endif;
+									?></a>
+									<?php echo excerpt(35); ?>
+									<a href="<?php echo get_permalink(); ?>"> mehr...</a>
+									<?php the_category(); ?>  
+								</div>
+
+							<?php endif; endwhile; else: ?>
+							<div>Alternate content</div>
+						<?php endif; ?>
+					</div>
+					<?php $i = 0; rewind_posts(); ?>
+					<div class="col col50">
+						<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
+
+							<div class="underline textwrap">
+								<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+								<?php if( get_field('subheadline') ): ?>
+									<h3><?php the_field('subheadline'); ?></h3>
+								<?php endif; ?>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+									<?php
+									if (class_exists('MultiPostThumbnails')) :
+										if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
+											MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
+										else : ?>
+										<?php the_post_thumbnail('large'); ?>
+										<?php
+										endif;
+										endif;
+										?></a>
+										<?php echo excerpt(35); ?>
+										<a href="<?php echo get_permalink(); ?>"> mehr...</a>
+										<?php the_category(); ?>  
+									</div>
+
+								<?php endif; endwhile; else: ?>
+								<div>Alternate content</div>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="col sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+
+		</section>
+		<!-- /section -->
 	
 <?php get_sidebar(); ?>
 
