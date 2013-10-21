12789d66677794eb4bb60049b99809b723e6c985
Chad Danford <chad.m.danford@gmail.com>
2013-10-21 20:08:38 +0200

    adsfasd

--- sidebar.php
+++ sidebar.php
@@ -19,8 +19,9 @@
 	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/News_Talks-and-Events_IBA-Logo.jpg" alt="" >
 	    					</a>
 	    				</figure>
+						<div class="expand">
+							<p></p>
+						</div>
 	    	</div>
-	    	<div class="expand">
-	    		<p></p>
-	    	</div>
+
 <!-- /sidebar -->
