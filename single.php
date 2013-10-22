d6379a2cb65115f2e684db201ee16c7303030e78
Chad Danford <chad.m.danford@gmail.com>
2013-10-22 12:18:20 +0200

    back to normal

--- single.php
+++ single.php
@@ -6,10 +6,10 @@
         <div class="col col100">
           <div class="module">
             <div class="textwrap categorysingle">
-<div class="testcontainer">                <h2><?php the_title(); ?></h2>
+                <h2><?php the_title(); ?></h2>
                 <?php if( get_field('subheadline') ): ?>
                     <h3><?php the_field('subheadline'); ?></h3>
-                  <?php endif; ?></div>
+                  <?php endif; ?>
               <?php the_content(); ?>
                 <?php the_category(); ?>              
             </div>
