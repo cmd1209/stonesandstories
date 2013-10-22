26b3d139565d3663f38aa692913bae1f55274a23
Chad Danford <chad.m.danford@gmail.com>
2013-10-22 15:23:06 +0200

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -2,20 +2,20 @@
 
 			<h2><span style="text-transform: uppercase; letter-spacing:0.2em;">Side</span>Notes</h2>
 	    				<figure>
-	    					<a href="http://www.humboldt-forum.de/humboldt-lab-dahlem/" target="blank">
+	    					<a class="thumbnail" href="http://www.humboldt-forum.de/humboldt-lab-dahlem/" target="blank">
 	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/Probebühne3.jpg" alt="" >
 	    					</a>
 	    				</figure>
 		
 
 	    				<figure>
-	    					<a href="http://www.dah-bremerhaven.de/australien.php" target="blank">
+	    					<a class="thumbnail" href="http://www.dah-bremerhaven.de/australien.php" target="blank">
 	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" >
 	    					</a>
 	    				</figure>
 
 						<figure>
-							<a href="http://www.iba-hamburg.de/" target="blank">
+							<a class="thumbnail" href="http://www.iba-hamburg.de/" target="blank">
 	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/News_Talks-and-Events_IBA-Logo.jpg" alt="" >
 	    					</a>
 	    				</figure>
