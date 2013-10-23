dccac0337ee0c4ecd91dac6d46b2548a290ca804
Chad Danford <chad.m.danford@gmail.com>
2013-10-23 10:50:51 +0200

    team spirit test

---
+++ team-spirit.php
@@ -0,0 +1,37 @@
+<?php /* Template Name: Team-Spirit */ get_header(); ?>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+  <section role="main">
+    <div class="col maincontent">    
+      <div class="col col100">
+        <div class="module">
+          <div class="textwrap">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+      <div class="col col50">
+             <?php $ids = array(168,1460,162,41,8,35); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 6) ); ?>
+              <?php while (have_posts()) : the_post(); ?>
+                <div class="textwrap">
+                  <h2><?php the_title(); ?></h2>
+                  <?php if( get_field('subheadline') ): ?>
+                    <h3><?php the_field('subheadline'); ?></h3>
+                  <?php endif; ?>
+                  <?php echo excerpt(200); ?>
+                  <a href="<?php echo get_permalink(); ?>"> mehr...</a>
+                </div>              
+      </div>
+     
+
+
+
+
+
+    </div>
+    <div class="sidebar">
+      <?php get_sidebar(); ?>
+    </div>
+  </section>
+  <?php endwhile; ?>
+  <?php endif; ?>
+<?php get_footer(); ?>
