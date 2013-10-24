91dfc9855fc0162d8f05557e07549b436252d3db
Chad Danford <chad.m.danford@gmail.com>
2013-10-24 17:45:59 +0200

    asdfads

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(function() {
     var closepanel = $('.factbox').children('.toggle').hide();
     $('.trigger').click(function(){ 
       closepanel.slideUp();
-      $('.factbox').toggleClass("trigger");
+      $('.trigger').toggleClass("trigger");
       $(this).closest('.factbox').children('.toggle').slideToggle("fast");
       $(this).toggleClass("triggerup");
       return false;
