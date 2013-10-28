f86f076431373e2b2f497b594039eb2df1efbf32
Chad Danford <chad.m.danford@gmail.com>
2013-10-28 12:22:42 +0100

    asdf

--- factsfigures.php
+++ factsfigures.php
@@ -12,7 +12,8 @@
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
-                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a><?php if( get_field('factsfigures') ): ?>
+                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                <?php if( get_field('factsfigures') ): ?>
                     <a class="trigger up" href="#"></a>
                 <?php endif; ?>
             </div>
@@ -43,7 +44,8 @@
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
-                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a><?php if( get_field('factsfigures') ): ?>
+                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                <?php if( get_field('factsfigures') ): ?>
                     <a class="trigger up" href="#"></a>
                 <?php endif; ?>
             </div>
@@ -75,7 +77,8 @@
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox">
             <div class="col col50" style="padding-right: 20px;">
-                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a><?php if( get_field('factsfigures') ): ?>
+                <a name="<?php the_title(); ?>" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                <?php if( get_field('factsfigures') ): ?>
                     <a class="trigger up" href="#"></a>
                 <?php endif; ?>
             </div>
