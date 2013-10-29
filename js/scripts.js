fad3dc86a2c1bc9a39bdb1b6cf1643de3dbe2116
Chad Danford <chad.m.danford@gmail.com>
2013-10-29 15:34:42 +0100

    adsfasdf

--- js/scripts.js
+++ js/scripts.js
@@ -39,14 +39,9 @@ $(function() {
 });
 
 $(function() {
-    var closepanel = $('.factbox').children('.toggle').hide();
     $('.facts').click(function(){ 
-      closepanel.slideUp();
-      $('.factbox').css('background-color','white');
-      $('.facts').removeClass("down").addClass("up");
       $(this).closest('.factbox').children('.toggle').slideToggle("fast");
       $(this).closest('.factbox').css('background-color','#e1e1e1');
-      $(this).removeClass( "up" ).addClass( "down" );
       return false;
     });
 });
