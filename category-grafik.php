05fc6db94651f59aa0ffd5761c138d00dcd25eda
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 15:49:44 +0100

    grafik

--- category-grafik.php
+++ category-grafik.php
@@ -5,28 +5,44 @@
 
         <div class="col col100">
           <div class="module" style="background-color:black;">
-          <div class="gdouble">
+<div class="col col50">
 
-				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-            				<li>
-            				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-            			<?php
-if (class_exists('MultiPostThumbnails')) :
-	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-	else : ?>
-		<?php the_post_thumbnail('large'); ?>
-<?php
-	endif;
-endif;
-?>
-</a>
-</li>
-					<?php endif; ?>
-					<?php endwhile; ?>
-					<?php endif; ?>
+					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+
+						<div class="textwrap underline">
+							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+							<?php if( get_field('subheadline') ): ?>
+                    			<h3><?php the_field('subheadline'); ?></h3>
+                 			 <?php endif; ?>
+							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
+							<?php echo excerpt(35); ?>
+							<a href="<?php echo get_permalink(); ?>"> mehr...</a>
+							<?php the_category(); ?>  
+						</div>
+
+					<?php endif; endwhile; else: ?>
+					<div>Alternate content</div>
+				<?php endif; ?>
+			</div>
+			<?php $i = 0; rewind_posts(); ?>
+			<div class="col col50">
+				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
+
+					<div class="underline textwrap">
+						<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<?php if( get_field('subheadline') ): ?>
+                    		<h3><?php the_field('subheadline'); ?></h3>
+                  		<?php endif; ?>
+						<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
+						<?php echo excerpt(35); ?>
+						<a href="<?php echo get_permalink(); ?>"> mehr...</a>
+						<?php the_category(); ?>  
 					</div>
+
+				<?php endif; endwhile; else: ?>
+				<div>Alternate content</div>
+			<?php endif; ?>
+		</div>
           </div>
         </div>
 
