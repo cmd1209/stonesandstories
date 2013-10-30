bbab83d3203754cd8593bc2ea8f6d5dd16d1c231
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 16:00:12 +0100

    Revert 2a511a5..e5618bd
    This rolls back to commit 2a511a5c612fb27abf93f2ff2ff8ecd68f78fc2f.

--- category-grafik.php
+++ category-grafik.php
@@ -5,12 +5,14 @@
 
         <div class="col col100">
           <div class="module" style="background-color:black;">
+          <div class="gdouble">
 
-          <div class="col col50">
-					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
-						<li>
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+            				<li>
             				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-            			<?php if (class_exists('MultiPostThumbnails')) :
+            			<?php
+if (class_exists('MultiPostThumbnails')) :
 	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
 		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
 	else : ?>
@@ -21,26 +23,10 @@ endif;
 ?>
 </a>
 </li>
-</div>
-			<?php $i = 0; rewind_posts(); ?>
-
-          <div class="col col50">
-					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
-						<li>
-            				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-            			<?php if (class_exists('MultiPostThumbnails')) :
-	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-	else : ?>
-		<?php the_post_thumbnail('large'); ?>
-<?php
-	endif;
-endif;
-?>
-</a>
-</li>
-</div>
-
+					<?php endif; ?>
+					<?php endwhile; ?>
+					<?php endif; ?>
+					</div>
           </div>
         </div>
 
