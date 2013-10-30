517e25dee9c7b9d6e9ca39df6bc8ee5383dfd079
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 11:08:26 +0100

    kdf

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
                 <?php if( get_field('subheadline') ): ?>
                     <h3><?php the_field('subheadline'); ?></h3>
                     
-                    <div class="factbox" id="<?php the_title(); ?>">
+                    <div class="factbox" style="margin-bottom: 30px;" id="<?php the_title(); ?>">
                       <?php if( get_field('factsfigures') ): ?>
                     <li class="post-categories facts"><a href="#">Facts & Figures</a></li>
                 <?php endif; ?>
