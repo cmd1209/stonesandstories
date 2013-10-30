9dc670929d0ae9b7008fc37c7a6d69514e96edb7
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 16:07:03 +0100

    back to normal

--- category-grafik.php
+++ category-grafik.php
@@ -5,28 +5,25 @@
 
         <div class="col col100">
           <div class="module" style="background-color:black;">
-          <div class="gdouble">
 
-				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-            				<li>
-            				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-            			<?php
-if (class_exists('MultiPostThumbnails')) :
-	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-	else : ?>
-		<?php the_post_thumbnail('large'); ?>
-<?php
-	endif;
-endif;
-?>
-</a>
-</li>
-					<?php endif; ?>
-					<?php endwhile; ?>
-					<?php endif; ?>
-					</div>
+          	<div class="gdouble">
+          		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+          			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+          				<li>
+          					<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
+          						<?php if (class_exists('MultiPostThumbnails')) :
+          						if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
+          							MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
+          						else : ?>
+          						<?php the_post_thumbnail('large'); ?>
+          					<?php endif; endif; ?>
+          				</a>
+          			</li>
+          		<?php endif; ?>
+          	<?php endwhile; ?>
+          <?php endif; ?>
+      </div>
+					
           </div>
         </div>
 
