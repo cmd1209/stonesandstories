e5618bd6567cc7ab632420b793f1b0f74c8e28d1
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 15:59:06 +0100

    omg hlep

--- category-grafik.php
+++ category-grafik.php
@@ -5,14 +5,12 @@
 
         <div class="col col100">
           <div class="module" style="background-color:black;">
-          <div class="col col50">
 
-				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
-            		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-            				<li>
+          <div class="col col50">
+					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+						<li>
             				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-            			<?php
-if (class_exists('MultiPostThumbnails')) :
+            			<?php if (class_exists('MultiPostThumbnails')) :
 	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
 		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
 	else : ?>
@@ -23,10 +21,26 @@ endif;
 ?>
 </a>
 </li>
-					<?php endif; ?>
-					<?php endwhile; ?>
-					<?php endif; ?>
-					</div>
+</div>
+			<?php $i = 0; rewind_posts(); ?>
+
+          <div class="col col50">
+					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+						<li>
+            				<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
+            			<?php if (class_exists('MultiPostThumbnails')) :
+	if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
+		MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
+	else : ?>
+		<?php the_post_thumbnail('large'); ?>
+<?php
+	endif;
+endif;
+?>
+</a>
+</li>
+</div>
+
           </div>
         </div>
 
