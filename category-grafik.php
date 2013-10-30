bff7a0948e3b853b8c2f43b1a2c31efadebde5aa
Chad Danford <chad.m.danford@gmail.com>
2013-10-30 16:15:30 +0100

    graphic second try

--- category-grafik.php
+++ category-grafik.php
@@ -6,24 +6,54 @@
         <div class="col col100">
           <div class="module" style="background-color:black;">
 
-          	<div class="gdouble">
-          		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-          			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-          				<li>
-          					<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
-          						<?php if (class_exists('MultiPostThumbnails')) :
+          <div class="col col50">
+
+					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+
+						<div class="textwrap underline">
+
+							<li>
+	<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
+	<?php if (class_exists('MultiPostThumbnails')) :
           						if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
           							MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
           						else : ?>
           						<?php the_post_thumbnail('large'); ?>
           					<?php endif; endif; ?>
-          				</a>
-          			</li>
-          		<?php endif; ?>
-          	<?php endwhile; ?>
-          <?php endif; ?>
-      </div>
-					
+	</a>
+</li>
+		
+						</div>
+
+					<?php endif; endwhile; else: ?>
+					<div>Alternate content</div>
+				<?php endif; ?>
+			</div>
+			<?php $i = 0; rewind_posts(); ?>
+			<div class="col col50">
+				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
+
+					<div class="underline textwrap">
+		
+
+						<li>
+	<a href="<?php the_permalink(); ?>" class="biglink thumbnail">
+	<?php if (class_exists('MultiPostThumbnails')) :
+          						if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
+          							MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
+          						else : ?>
+          						<?php the_post_thumbnail('large'); ?>
+          					<?php endif; endif; ?>
+	</a>
+</li>
+
+					</div>
+
+				<?php endif; endwhile; else: ?>
+				<div>Alternate content</div>
+			<?php endif; ?>
+		</div>
+
           </div>
         </div>
 
