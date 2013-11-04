a5702ade7dbd0371e36ae6853f8e61b434fab52e
Chad Danford <chad.m.danford@gmail.com>
2013-11-04 16:14:42 +0100

    reverse order

--- single.php
+++ single.php
@@ -13,8 +13,8 @@
                     <div class="factbox" style="margin-bottom: 30px;" id="<?php the_title(); ?>">
                       <?php if( get_field('factsfigures') ): ?>
                     <ul class="post-categories facts">
-                      <li><a id="factstoggle" href="#">Facts & Figures</a></li>
                       <li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li>
+                      <li><a id="factstoggle" href="#">Facts & Figures</a></li>
                     </ul>
                     
                   <?php endif; ?>          
