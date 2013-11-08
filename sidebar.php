4fa1f82060d81f12a31db06ba6216bb6a3a396e0
Chad Danford <chad.m.danford@gmail.com>
2013-11-08 15:37:26 +0100

    reindent sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,33 @@
 <!-- sidebar -->
-			<h2><span style="text-transform: uppercase; letter-spacing:0.2em; font-size: 150%;">Side</span>notes</h2>
+<h2><span style="text-transform: uppercase; letter-spacing:0.2em; font-size: 150%;">Side</span>notes</h2>
 
-	    				<figure>
-	    					<a class="thumbnail" href="http://www.junius-verlag.de/buecher/architektur-hamburg-jahrbuch-2013" target="blank">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/jahrbuch-2013.jpg" alt="" >
-	    					</a>
-	    				</figure>
+<figure>
+	<a class="thumbnail" href="http://www.junius-verlag.de/buecher/architektur-hamburg-jahrbuch-2013" target="blank">
+		<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/jahrbuch-2013.jpg" alt="" >
+	</a>
+</figure>
 
-	    				<figure>
-	    					<a class="thumbnail" href="http://www.dah-bremerhaven.de/australien.php" target="blank">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" >
-	    					</a>
-	    				</figure>
+<figure>
+	<a class="thumbnail" href="http://www.dah-bremerhaven.de/australien.php" target="blank">
+		<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/australian-poster.jpg" alt="" >
+	</a>
+</figure>
 
-						<figure>
-							<a class="thumbnail" href="http://www.iba-hamburg.de/" target="blank">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/News_Talks-and-Events_IBA-Logo.jpg" alt="" >
-	    					</a>
-	    				</figure>
+<figure>
+	<a class="thumbnail" href="http://www.iba-hamburg.de/" target="blank">
+		<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/09/News_Talks-and-Events_IBA-Logo.jpg" alt="" >
+	</a>
+</figure>
 
-	    				<figure>
-							<a class="thumbnail" href="http://www.waelderhaus.de/architektier/" target="blank">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/architechtier.jpg" alt="" >
-	    					</a>
-	    				</figure>
+<figure>
+	<a class="thumbnail" href="http://www.waelderhaus.de/architektier/" target="blank">
+		<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/architechtier.jpg" alt="" >
+	</a>
+</figure>
 
-	    				<figure>
-	    					<a class="thumbnail" href="http://www.humboldt-forum.de/humboldt-lab-dahlem/" target="blank">
-	    						<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/Probebühne3.jpg" alt="" >
-	    					</a>
-	    				</figure>
+<figure>
+	<a class="thumbnail" href="http://www.humboldt-forum.de/humboldt-lab-dahlem/" target="blank">
+		<img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/Probebühne3.jpg" alt="" >
+	</a>
+</figure>
 <!-- /sidebar -->
