91661c5f1652a2ee947e4072ed0a0516ee3ca8d6
Chad Danford <chad.m.danford@gmail.com>
2013-11-11 12:39:51 +0100

    search form

--- header.php
+++ header.php
@@ -34,12 +34,12 @@
           <div class="atmosphere">
             <a href="<?php echo get_permalink(127); ?>" class="filmbutton"><img src="http://img.youtube.com/vi/KGsHXZjj04Q/mqdefault.jpg" alt=""></a>
           </div>
+              <?php get_template_part('searchform'); ?>
         </div>
         <div class="navigation">
 
           <div class="bmenu wrapper" role="navigation">
               <?php wp_nav_menu( array('menu' => 'Black' )); ?>
-              <?php get_template_part('searchform'); ?>
           </div>
         </div>
       <div class="wrapper" id="stage">
