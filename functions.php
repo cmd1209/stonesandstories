2013584f9288c0bca5f73d1baba098cc2f4297f9
Chad Danford <chad.m.danford@gmail.com>
2013-11-12 15:54:04 +0100

    functions

--- functions.php
+++ functions.php
@@ -486,6 +486,20 @@ function excerpt($limit) {
       return $excerpt;
     }
 
+    function content($limit) {
+      $content = explode(' ', get_the_content(), $limit);
+      if (count($content)>=$limit) {
+        array_pop($content);
+        $content = implode(" ",$content).'...';
+      } else {
+        $content = implode(" ",$content);
+      } 
+      $content = preg_replace('/\[.+\]/','', $content);
+      $content = apply_filters('the_content', $content); 
+      $content = str_replace(']]>', ']]&gt;', $content);
+      return $content;
+    }
+
 /**
  * Swap P Tags for Figure Tags on Images
  */
