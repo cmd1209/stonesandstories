442f1dfa6fe1d034e37410b206f5cca1a455ecb4
Chad Danford <chad.m.danford@gmail.com>
2013-11-13 11:39:29 +0100

    test

--- start.php
+++ start.php
@@ -35,7 +35,7 @@
           <div class="module">
              <?php $ids = array(751); query_posts(array('orderby' => 'rand', 'post_type' => 'post', 'post__in' => $ids, 'showposts' => 1) ); ?>
               <?php while (have_posts()) : the_post(); ?>
-              <div class="col100" style="z-index:10; background-image: url(<?php echo get_the_post_thumbnail($large->ID, 'large');?>);">
+              <div class="col60" style="z-index:10;">
               <div class="moduleheader">
                 <h2>Bühnenbild für<br>Das St. Pauli-Theater   <!-- manually added -->
                 <?php if( get_field('subheadline') ): ?>
@@ -46,9 +46,9 @@
                     <?php echo excerpt(32); ?>
                   </div>
               </div>
-<!--                <div class="heroimage shift2 yellow"><a class="thumbnail" href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a></div>  -->
-            <?php endwhile; ?>
           </div>
+               <div class="heroimage shift2 yellow"><a class="thumbnail" href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($large->ID, 'large');?></a></div> 
+            <?php endwhile; ?>
         </div>
        
         <div class="col col100 H300 S">
