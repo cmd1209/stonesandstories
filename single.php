50771393dc0f32e07a50ee9bb353fa001d2e0b2b
Chad Danford <chad.m.danford@gmail.com>
2013-11-13 15:56:47 +0100

    background color black

--- single.php
+++ single.php
@@ -12,7 +12,7 @@
                <span><?php the_field('subheadline'); ?></span></h2>
               </div>   
                   
-                    <div class="factbox" style="margin-bottom: 30px;padding-left:0;" id="<?php the_title(); ?>">
+                    <div class="factbox" style="margin-bottom: 30px;padding-left:0;background-color:black;" id="<?php the_title(); ?>">
                     <ul class="post-categories facts" style="padding-left:0;">
                       <?php if( get_field('factsfigures') ): ?>
                       <li><a id="factstoggle" href="#">Facts & Figures</a></li>      <?php endif; ?>
