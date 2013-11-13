e457569d384bdb68b2a735484a382dbc9b218383
Chad Danford <chad.m.danford@gmail.com>
2013-11-13 16:06:12 +0100

    bac

--- single.php
+++ single.php
@@ -1,45 +1,43 @@
 <?php get_header(); ?>
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main">
     <div class="col maincontent" style="min-height:1100px;">
       
-      <div class="col col100">
-        <div class="module">
-          <div class="categorysingle">
-            <div class="moduleheader col50">
-              <h2><?php the_title(); ?>
+        <div class="col col100">
+          <div class="module">
+            <div class="textwrap categorysingle">
+                <div class="moduleheader">
+                <h2><?php the_title(); ?>
                 <?php if( get_field('subheadline') ): ?>
-                 <span><?php the_field('subheadline'); ?></span></h2>
-               </div>   
-               
-               <div class="factbox" style="margin-bottom: 30px;padding-left:0;background-color:black;" id="<?php the_title(); ?>">
-                <ul class="post-categories facts" style="padding-left:0;">
-                  <?php if( get_field('factsfigures') ): ?>
-                    <li><a id="factstoggle" href="#">Facts & Figures</a></li>      <?php endif; ?>
-                    <li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?>   
-                  </ul>    
+               <span><?php the_field('subheadline'); ?></span></h2>
+              </div>   
                   
-                  <div class="toggle" style="padding-left:0;">
-                   <?php if( get_field('factsfigures') ): ?>
+                    <div class="factbox" style="margin-bottom: 30px;padding-left:0;" id="<?php the_title(); ?>">
+                    <ul class="post-categories facts" style="padding-left:0;">
+                      <?php if( get_field('factsfigures') ): ?>
+                      <li><a id="factstoggle" href="#">Facts & Figures</a></li>      <?php endif; ?>
+                      <li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?>   
+                    </ul>    
+        
+              <div class="toggle" style="padding-left:0;">
+               <?php if( get_field('factsfigures') ): ?>
                     <?php the_field('factsfigures'); ?>
+                <?php endif; ?>
+              </div>     
+          </div>
                   <?php endif; ?>
-                </div>     
-              </div>
-            <?php endif; ?>
-            <div class="textwrap">
-              <?php the_content(); ?>
-            </div> 
+              <?php the_content(); ?>   
+            </div>
           </div>
         </div>
-      </div>
 
     </div>
     <div class="sidebar">
       <?php get_sidebar(); ?>
     </div>
   </section>
-<?php endwhile; ?>
-<?php endif; ?>
+  <?php endwhile; ?>
+  <?php endif; ?>
 <?php get_footer(); ?>
 
 
