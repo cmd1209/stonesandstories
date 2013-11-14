c58eaf8e22df9bdac8064dfa22f5a89942fdea40
Chad Danford <chad.m.danford@gmail.com>
2013-11-14 10:18:03 +0100

    asdfa

--- category.php
+++ category.php
@@ -12,12 +12,9 @@
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
 						<div class="textwrap underline">
-														<div class="singletitle">
-  <h2><?php the_title(); ?>
-  <?php if( get_field('subheadline') ): ?>
-    <span><?php the_field('subheadline'); ?></span>
-  </h2>
-</div> 
+							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+							<?php if( get_field('subheadline') ): ?>
+								<h3><?php the_field('subheadline'); ?></h3>
 							<?php endif; ?>
 							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
 								<?php
@@ -42,12 +39,9 @@
 						<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
 
 							<div class="underline textwrap">
-															<div class="singletitle">
-  <h2><?php the_title(); ?>
-  <?php if( get_field('subheadline') ): ?>
-    <span><?php the_field('subheadline'); ?></span>
-  </h2>
-</div> 
+								<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+								<?php if( get_field('subheadline') ): ?>
+									<h3><?php the_field('subheadline'); ?></h3>
 								<?php endif; ?>
 								<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
 									<?php
