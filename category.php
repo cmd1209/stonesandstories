a376b986305784efc316c43f927ef4894db585fb
Chad Danford <chad.m.danford@gmail.com>
2013-11-14 10:19:44 +0100

    spanken

--- category.php
+++ category.php
@@ -12,63 +12,51 @@
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
 						<div class="textwrap underline">
-							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-							<?php if( get_field('subheadline') ): ?>
-								<h3><?php the_field('subheadline'); ?></h3>
-							<?php endif; ?>
-							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
-								<?php
-								if (class_exists('MultiPostThumbnails')) :
-									if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-										MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-									else : ?>
-									<?php the_post_thumbnail('large'); ?>
-									<?php
-									endif;
-									endif;
-									?></a>
-									<?php echo excerpt(35); ?>
-								</div>
-
-							<?php endif; endwhile; else: ?>
-							<div>Alternate content</div>
-						<?php endif; ?>
-					</div>
-					<?php $i = 0; rewind_posts(); ?>
-					<div class="col col50">
-						<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-
-							<div class="underline textwrap">
-								<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-								<?php if( get_field('subheadline') ): ?>
-									<h3><?php the_field('subheadline'); ?></h3>
-								<?php endif; ?>
-								<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
-									<?php
-									if (class_exists('MultiPostThumbnails')) :
-										if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-											MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-										else : ?>
-										<?php the_post_thumbnail('large'); ?>
-										<?php
-										endif;
-										endif;
-										?></a>
-										<?php echo excerpt(35); ?>
-									</div>
-
-								<?php endif; endwhile; else: ?>
-								<div>Alternate content</div>
-							<?php endif; ?>
+							<div class="singletitle">
+  <h2><?php the_title(); ?>
+  <?php if( get_field('subheadline') ): ?>
+    <span><?php the_field('subheadline'); ?></span>
+  </h2>
+</div> 
+                 			 <?php endif; ?>
+							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
+							<?php echo excerpt(40); ?>
 						</div>
+
+					<?php endif; endwhile; else: ?>
+					<div>Alternate content</div>
+				<?php endif; ?>
+			</div>
+			<?php $i = 0; rewind_posts(); ?>
+			<div class="col col50">
+				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
+
+					<div class="underline textwrap">
+						<div class="singletitle">
+  <h2><?php the_title(); ?>
+  <?php if( get_field('subheadline') ): ?>
+    <span><?php the_field('subheadline'); ?></span>
+  </h2>
+</div> 
+                  		<?php endif; ?>
+						<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
+						<?php echo excerpt(40); ?>
 					</div>
-				</div>
 
-			</div>
-			<div class="sidebar">
-				<?php get_sidebar(); ?>
-			</div>
+				<?php endif; endwhile; else: ?>
+				<div>Alternate content</div>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
 
-		</section>
-		<!-- /section -->
-		<?php get_footer(); ?>
+</div>
+<div class="sidebar">
+	<?php get_sidebar(); ?>
+</div>
+
+</section>
+<!-- /section -->
+
+
+<?php get_footer(); ?>
