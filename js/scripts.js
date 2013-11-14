5a21deadabaa0e58d76c964ac2f193bc94e6f7d4
Chad Danford <chad.m.danford@gmail.com>
2013-11-14 11:35:47 +0100

    sadfadsf

--- js/scripts.js
+++ js/scripts.js
@@ -45,14 +45,17 @@ $(function() {
 
 $(function(){
         var stickynavtop = $('.navigation').offset().top;
-        var stage = $('#stage');         
+        var stage = $('#stage');
+        var atmosphere = $('.atmosphere');         
         $(window).scroll(function(){
                 if( $(window).scrollTop() > stickynavtop ) {
                         $('.navigation').css({position: 'fixed', top: '0px'});
+                        atmosphere.fadeOut();
                         stage.css({"padding-top":"50px"});
                 } else {
                         $('.navigation').css({position: 'static', top: '0px'});
                         stage.css({"padding-top":"0px"});
+                        atmosphere.fadeIn();
                 }
         });
 });
