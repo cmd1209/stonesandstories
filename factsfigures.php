162216e954fe4fae2be32420fbecaa5571f0d27c
Chad Danford <chad.m.danford@gmail.com>
2013-11-15 10:34:25 +0100

    multiple projects

--- factsfigures.php
+++ factsfigures.php
@@ -7,8 +7,7 @@
         <h1><span><?php the_title(); ?></span></h1>
 
           <div class="textwrap">
-          <?php query_posts( array( 'category__in' => array(60), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
-            <h2>Architecture</h2>
+          <?php query_posts( array( 'category__in' => array(60,12,11), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox" id="<?php the_title(); ?>">
             <div style="width:100%;overflow:hidden;">
@@ -46,85 +45,6 @@
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>  
           </div>   
-
-          <div class="textwrap">
-          <?php query_posts( array( 'category__in' => array(11), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
-            <h2>Ausstellungsdesign</h2>
-          <?php while (have_posts()) : the_post(); ?>
-          <div class="factbox" id="<?php the_title(); ?>">
-          <div style="width:100%;overflow:hidden;">
-          <div class="col col45" style="padding-right: 20px;">
-                          <?php if( get_field('factsfigures') ): ?>
-                              <a class="trigger up" href="#"></a>
-                            <?php else: ?>
-                              <a class="blanktrigger" href="#"></a>
-                          <?php endif; ?>
-                          <?php if($post->post_content != "") : ?>
-                            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-                          <?php else: ?>
-                            <a href="#"><?php the_title(); ?></a>
-                          <?php endif; ?>
-                      </div>
-                      <div class="col col5">
-                        <?php the_date('Y', '<p>', '</p>'); ?>
-                      </div>
-                      <div class="col col20" style="min-height: 1px;">
-                         <?php if( get_field('location') ): ?>
-                              <?php the_field('location'); ?>
-                          <?php endif; ?>
-                      </div>
-                      <div class="col col30">
-                        <ul class="post-categories"><li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?></ul>
-                      </div></div>
-              <div class="toggle">
-               <?php if( get_field('factsfigures') ): ?>
-                    <?php the_field('factsfigures'); ?>
-                <?php endif; ?>
-              </div>     
-          </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>
-          </div>
-
-
-          <div class="textwrap">
-          <?php query_posts( array( 'category__in' => array(12), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
-            <h2>Grafikdesign</h2>
-          <?php while (have_posts()) : the_post(); ?>
-          <div class="factbox" id="<?php the_title(); ?>">
-            <div style="width:100%;overflow:hidden;">
-            <div class="col col45" style="padding-right: 20px;">
-                            <?php if( get_field('factsfigures') ): ?>
-                                <a class="trigger up" href="#"></a>
-                              <?php else: ?>
-                                <a class="blanktrigger" href="#"></a>
-                            <?php endif; ?>
-                            <?php if($post->post_content != "") : ?>
-                              <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-                            <?php else: ?>
-                              <a href="#"><?php the_title(); ?></a>
-                            <?php endif; ?>
-                        </div>
-                        <div class="col col5">
-                          <?php the_date('Y', '<p>', '</p>'); ?>
-                        </div>
-                        <div class="col col20" style="min-height: 1px;">
-                           <?php if( get_field('location') ): ?>
-                                <?php the_field('location'); ?>
-                            <?php endif; ?>
-                        </div>
-                        <div class="col col30">
-                          <ul class="post-categories"><li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?></ul>
-                        </div></div>
-              <div class="toggle">
-               <?php if( get_field('factsfigures') ): ?>
-                    <?php the_field('factsfigures'); ?>
-                <?php endif; ?>
-              </div>     
-          </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>
-          </div>     
 
 
     </div>
