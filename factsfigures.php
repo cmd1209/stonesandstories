ab4c57a1125aa614cdd26779ec4afad19c872026
Chad Danford <chad.m.danford@gmail.com>
2013-11-15 11:26:25 +0100

    asdfad

--- factsfigures.php
+++ factsfigures.php
@@ -32,7 +32,7 @@
 
               </div>
               <div class="col100">
-                <ul class="post-categories"><li><a href="#" class="trigger">facts & figures</a></li><li style="float:left;"><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?></ul>
+                <ul class="post-categories"><li style="float:left;"><a href="#" class="trigger">facts & figures</a><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?></ul>
               </div>     
               <div class="toggle">
                <?php if( get_field('factsfigures') ): ?>
