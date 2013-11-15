9f1704e1e088350ed798f7718bd8a804dc8fc3bb
Chad Danford <chad.m.danford@gmail.com>
2013-11-15 11:16:12 +0100

    asdfsda

--- factsfigures.php
+++ factsfigures.php
@@ -18,7 +18,7 @@
                                 <a class="blanktrigger" href="#"></a>
                             <?php endif; ?>
                             <?php if($post->post_content != "") : ?>
-                              <a style="text-transform:uppercase:font-weight:300;" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                              <a style="text-transform:uppercase;font-weight:300;" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
                             <?php else: ?>
                               <a href="#"><?php the_title(); ?></a>
                             <?php endif; ?>
@@ -40,7 +40,7 @@
                     <?php the_field('factsfigures'); ?>
                 <?php endif; ?>
               </div>
-              <div class="col100">
+              <div class="col100" style="background-color: rgb(70,70,70);overflow:hidden;">
                 <ul class="post-categories"><li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?></ul>
               </div>     
           </div>
