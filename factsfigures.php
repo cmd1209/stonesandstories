633723d06ad726740305b2d0487db4036ca0d5de
Chad Danford <chad.m.danford@gmail.com>
2013-11-15 14:51:41 +0100

    sdfd

--- factsfigures.php
+++ factsfigures.php
@@ -21,7 +21,13 @@
               <li><?php if( get_field('location') ): ?><?php the_field('location'); ?><?php endif; ?></li>
           </div>
           <div class="col100">
-                <div class="post-categories" style="overflow:hidden;margin-bottom:0px;"><li><?php if( get_field('factsfigures') ): ?><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?><?php endif; ?><a href="#" class="trigger">facts & figures</a><?php endif; ?></li></div>
+                <div class="post-categories" style="overflow:hidden;margin-bottom:0px;">
+                <li>
+                <?php if( get_field('custom-links') ): ?>
+                  <?php the_field('custom-links'); ?>
+                <?php endif; ?><a href="#" class="trigger">facts & figures</a>
+                </li>
+                </div>
               </div>     
               <div class="toggle">
                <?php if( get_field('factsfigures') ): ?>
