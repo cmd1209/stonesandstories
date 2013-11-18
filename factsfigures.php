ded805262f0ce842f81944accc471ba2246ffe02
Chad Danford <chad.m.danford@gmail.com>
2013-11-18 17:12:08 +0100

    lets see

--- factsfigures.php
+++ factsfigures.php
@@ -7,6 +7,54 @@
         <h1><span><?php the_title(); ?></span></h1>
           <div class="textwrap">
 
+          <div class="col100">
+            <h2>Aktuelle Projekte</h2>
+          <?php query_posts( array( 'category__in' => array(88), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
+          <?php while (have_posts()) : the_post(); ?>
+          <div class="factbox" id="<?php the_title(); ?>">
+          <div class="factsheader">
+          <div class="col col2">
+            <a href="#" class="trigger"></a>
+          </div>
+          <div class="col col55">
+            <?php if($post->post_content != "") : ?>
+              <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+              <?php else: ?>
+              <a href="#" class="title"><?php the_title(); ?></a>
+              <?php endif; ?>
+          </div>
+          <div class="col col5">
+            <p>Aktuell</p>
+          </div>
+          <div class="col col10">
+            <?php if( get_field('location') ): ?>
+              <p><?php the_field('location'); ?></p>
+            <?php endif; ?>
+          </div>
+          <div class="col col28">
+            <ul class="post-categories">
+              <li>
+            <?php if( get_field('divsions') ): ?>
+                  <?php the_field('divsions'); ?>
+            <?php endif; ?>
+              </li>
+            </ul>
+          </div>
+          </div>
+   
+              <div class="toggle">
+               <?php if( get_field('factsfigures') ): ?>
+                    <?php the_field('factsfigures'); ?>
+                <?php endif; ?>
+              </div>
+          </div>
+          <?php endwhile; ?>
+          <?php wp_reset_query(); ?>  
+          </div> 
+          </div>
+
+                    <div class="col100">
+            <h2>Projekte</h2>
           <?php query_posts( array( 'category__in' => array(60,12,11), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
           <div class="factbox" id="<?php the_title(); ?>">
@@ -48,6 +96,7 @@
           </div>
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>  
+          </div> 
           </div>   
 
 
