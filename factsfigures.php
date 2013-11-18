9e8b3a05ae2112f9c1df834e04f42ad757a18c65
Chad Danford <chad.m.danford@gmail.com>
2013-11-18 12:38:21 +0100

    test2

--- factsfigures.php
+++ factsfigures.php
@@ -5,30 +5,34 @@
       <div class="col col100">
         <div class="module">
         <h1><span><?php the_title(); ?></span></h1>
-
           <div class="textwrap">
+
           <?php query_posts( array( 'category__in' => array(60,12,11), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-          
           <div class="factbox" id="<?php the_title(); ?>">
-          <div style="width:100%;overflow:hidden;" class="factsheader">
+          <div class="factsheader">
+          <div class="col60">
             <?php if($post->post_content != "") : ?>
               <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
               <?php else: ?>
               <a href="#"><?php the_title(); ?></a>
               <?php endif; ?>
-              <li><?php the_date('Y'); ?></li>
-              <li><?php if( get_field('location') ): ?><?php the_field('location'); ?><?php endif; ?></li>
           </div>
-          <div class="col100">
-                <div class="post-categories" style="overflow:hidden;margin-bottom:0px;">
-                <li>
-                <?php if( get_field('custom-links') ): ?>
-                  <?php the_field('custom-links'); ?>
-                <?php endif; ?><a href="#" class="trigger">facts & figures</a>
-                </li>
-                </div>
-              </div>     
+          <div class="col5">
+            <?php the_date('Y'); ?>
+          </div>
+          <div class="col5">
+            <?php if( get_field('location') ): ?>
+              <?php the_field('location'); ?>
+            <?php endif; ?>
+          </div>
+          <div class="col30">
+            <?php if( get_field('divisions') ): ?>
+                  <?php the_field('divisions'); ?>
+            <?php endif; ?>
+          </div>
+          </div>
+   
               <div class="toggle">
                <?php if( get_field('factsfigures') ): ?>
                     <?php the_field('factsfigures'); ?>
