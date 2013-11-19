0b3b2dbea2dd29b39933e340cbc0b228ea4e6512
Chad Danford <chad.m.danford@gmail.com>
2013-11-19 14:54:25 +0100

    adsfads

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
           </div>
         </div>
     </div>
-        <div class="navigation">
+        <div class="navigation wrapper">
           <div class="bmenu wrapper" role="navigation">
               <?php wp_nav_menu( array('menu' => 'Black' )); ?>
               <?php get_template_part('searchform'); ?>
