4608906aba4e080853cec526fde4564a8c07feda
Chad Danford <chad.m.danford@gmail.com>
2013-11-20 13:06:51 +0100

    armno test

--- header.php
+++ header.php
@@ -6,12 +6,14 @@
     
     <!-- dns prefetch -->
     <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="//fonts.googleapis.com/css?family=Arimo:400italic,400,700italic,700" rel="stylesheet" type="text/css">
+
     
     <!-- meta -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    
+
     <!-- icons -->
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
