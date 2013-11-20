9de610066b32e258c0e637daa6dd7a8a7e53ab50
Chad Danford <chad.m.danford@gmail.com>
2013-11-20 15:54:46 +0100

    adsf

--- category-kultur.php
+++ category-kultur.php
@@ -11,8 +11,13 @@
 
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
 
-						<div class="textwrap underline">
-							<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<div class="textwrap ">
+														<div class="singletitle">
+								<h2><?php the_title(); ?>
+									<?php if( get_field('subheadline') ): ?>
+										<span><?php the_field('subheadline'); ?></span>
+									</h2>
+								</div> 
 							<?php if( get_field('subheadline') ): ?>
                     			<h3><?php the_field('subheadline'); ?></h3>
                  			 <?php endif; ?>
@@ -28,8 +33,13 @@
 			<div class="col col50">
 				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
 
-					<div class="underline textwrap">
-						<h2> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+					<div class=" textwrap">
+													<div class="singletitle">
+								<h2><?php the_title(); ?>
+									<?php if( get_field('subheadline') ): ?>
+										<span><?php the_field('subheadline'); ?></span>
+									</h2>
+								</div> 
 						<?php if( get_field('subheadline') ): ?>
                     		<h3><?php the_field('subheadline'); ?></h3>
                   		<?php endif; ?>
