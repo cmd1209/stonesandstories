e8e7a5d06b3353e9d4bbdf643d35753b83cdf33a
Chad Danford <chad.m.danford@gmail.com>
2013-11-27 15:01:56 +0100

    test

--- category-architecture.php
+++ category-architecture.php
@@ -1,15 +1,12 @@
 <?php get_header(); ?>
-
-<!-- section -->
 <section role="main">
-
 	<div class="col maincontent">
 		<div class="col col100">
 			<div class="module">
 				<h1><strong><?php single_cat_title(); ?></strong></h1>
 				<div class="col col50">
 					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
-						<div class="textwrap">
+						<div class="textwrap ">
 							<div class="singletitle">
 								<h2><?php the_title(); ?>
 									<?php if( get_field('subheadline') ): ?>
@@ -18,9 +15,8 @@
 								</div> 
 							<?php endif; ?>
 							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-							<?php echo excerpt(40); ?>
+							<?php echo excerpt(35); ?>
 						</div>
-
 					<?php endif; endwhile; else: ?>
 					<div>Alternate content</div>
 				<?php endif; ?>
@@ -28,8 +24,7 @@
 			<?php $i = 0; rewind_posts(); ?>
 			<div class="col col50">
 				<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-
-					<div class="textwrap">
+					<div class=" textwrap">
 						<div class="singletitle">
 							<h2><?php the_title(); ?>
 								<?php if( get_field('subheadline') ): ?>
@@ -38,23 +33,16 @@
 							</div> 
 						<?php endif; ?>
 						<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>"><?php the_post_thumbnail('large'); ?></a>
-						<?php echo excerpt(40); ?>
+						<?php echo excerpt(35); ?>
 					</div>
-
 				<?php endif; endwhile; else: ?>
 				<div>Alternate content</div>
 			<?php endif; ?>
 		</div>
 	</div>
 </div>
-
 </div>
 <div class="sidebar">
 	<?php get_sidebar(); ?>
 </div>
-
 </section>
-<!-- /section -->
-
-
-<?php get_footer(); ?>
