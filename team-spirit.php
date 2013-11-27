74982aca6fab65c779b887312b02b8109cb5e695
Chad Danford <chad.m.danford@gmail.com>
2013-11-27 12:03:34 +0100

    second try

--- team-spirit.php
+++ team-spirit.php
@@ -27,12 +27,10 @@ if ( $parent->have_posts() ) : ?>
     <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
 
         <div class="col col50" id="post-<?php the_ID(); ?>">
-        <div class="module">
           <h1><span><?php the_title(); ?></span></h1>
         <div class="textwrap" id="<?php echo $page->post_name; ?>">
             <?php echo excerpt(150); ?>
             <a href="<?php echo get_permalink(); ?>"> mehr...</a>
-        </div>
         </div>
 
         </div>
