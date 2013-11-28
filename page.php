6024c765c3979a5d61586cf24e036dc11f16d5d7
Chad Danford <chad.m.danford@gmail.com>
2013-11-28 16:19:48 +0100

    safdad

--- page.php
+++ page.php
@@ -5,20 +5,26 @@
       
       <div class="col col100">
         <div class="module">
-          <h1><strong><?php the_title(); ?></strong></h1>
-          <div class="textwrap">
-            <?php the_content(); ?>
-            <div class="col col100">
-             <ul class="post-categories">
-                  <?php if( get_field('custom-links') ): ?>
-                    <li> <?php the_field('custom-links'); ?></li>
-                  <?php endif; ?>   
-                </ul> 
-            </div>
+          <div class="textwrap categorysingle">
+            <div class="singletitle">
+              <h2><?php the_title(); ?>
+                <?php if( get_field('subheadline') ): ?>
+                 <span><?php the_field('subheadline'); ?></span></h2>
+                <?php endif; ?>
+               </div>   
+
+               <div class="factbox" style="margin-bottom: 30px;padding-left:0;" id="<?php the_title(); ?>">
+                <ul class="post-categories facts" style="padding-left:0; overflow:hidden;">
+                  <?php if( get_field('factsfigures') ): ?>
+                    <li><a id="factstoggle" href="#">Facts & Figures</a></li><?php endif; ?>
+                    <li><?php if( get_field('custom-links') ): ?> <?php the_field('custom-links'); ?></li><?php endif; ?>   
+                  </ul>        
+              </div>
+
+            <?php the_content(); ?>   
           </div>
         </div>
       </div>
-
     </div>
     <div class="sidebar">
       <?php get_sidebar(); ?>
