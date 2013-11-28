cdf8edb05af7bcc8888db8b9769432cca7ea31ed
Chad Danford <chad.m.danford@gmail.com>
2013-11-28 10:22:45 +0100

    test

---
+++ factsfigures-bk.php
@@ -0,0 +1,103 @@
+<?php /* Template Name: factsfigures */ get_header(); ?>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+  <section role="main">
+    <div class="col maincontent">
+      <div class="col col100">
+        <div class="module">
+        <h1><strong><?php the_title(); ?></strong></h1>
+          <div class="textwrap">
+          <div class="col100">
+          <?php query_posts( array( 'category__in' => array(88), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'ASC' ) ); ?>
+          <?php while (have_posts()) : the_post(); ?>
+          <div class="factbox">
+          <div class="factsheader">
+          <div class="col tab2">
+            <?php if($post->post_content != "") : ?>
+              <a href="#" class="trigger"></a>
+              <?php else: ?>
+              <a href="#"></a>
+              <?php endif; ?>
+          </div>
+          <div class="col tab50">
+              <p class="title"><?php the_title(); ?></p>
+          </div>
+          <div class="col tab5">
+            <p>Aktuell</p>
+          </div>
+          <div class="col tab15">
+              <p><?php the_field('location'); ?></p>
+          </div>
+          <div class="col tab28">
+            <ul class="post-categories">
+              <li>
+            <?php if( get_field('divsions') ): ?>
+                  <?php the_field('divsions'); ?>
+            <?php endif; ?>
+              </li>
+            </ul>
+          </div>
+          </div>
+              <div class="toggle">
+               <?php if( get_field('factsfigures') ): ?>
+                    <?php the_field('factsfigures'); ?>
+                <?php endif; ?>
+              </div>
+          </div>
+          <?php endwhile; ?>
+          <?php wp_reset_query(); ?>  
+          </div> 
+
+          <?php query_posts( array( 'category__in' => array(60,11,42,95), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC', 'post__not_in' => array(73,2141,225,1605,1637,1844))); ?>
+          <?php while (have_posts()) : the_post(); ?>
+          <div class="factbox">
+          <div class="factsheader">
+          <div class="col tab2">
+              <?php if( get_field('factsfigures') ): ?>
+                  <a href="#" class="trigger"></a>
+              <?php else: ?>
+              <a href="#"></a>
+              <?php endif; ?>
+          </div>
+          <div class="col tab50">
+              <p class="title"><?php the_title(); ?></p>
+          </div>
+          <div class="col tab5">
+            <p><?php the_date('Y'); ?></p>
+          </div>
+          <div class="col tab15">
+              <p><?php the_field('location'); ?></p>
+          </div>
+          <div class="col tab28">
+            <ul class="post-categories">
+              <li>
+            <?php if( get_field('divsions') ): ?>
+                  <?php the_field('divsions'); ?>
+            <?php endif; ?>
+              </li>
+            </ul>
+          </div>
+          </div>
+   
+              <div class="toggle">
+               <?php if( get_field('factsfigures') ): ?>
+                    <?php the_field('factsfigures'); ?>
+                <?php endif; ?>
+              </div>
+          </div>
+          <?php endwhile; ?>
+          <?php wp_reset_query(); ?>  
+          </div> 
+
+
+
+    </div>
+    </div>
+    </div>
+
+    <div class="sidebar">
+      <?php get_sidebar(); ?>
+    </div>
+  </section>
+  <?php endwhile; ?>
+  <?php endif; ?>
+<?php get_footer(); ?>
--- factsfigures.php
+++ factsfigures.php
@@ -9,86 +9,44 @@
           <div class="col100">
           <?php query_posts( array( 'category__in' => array(88), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'ASC' ) ); ?>
           <?php while (have_posts()) : the_post(); ?>
-          <div class="factbox">
-          <div class="factsheader">
-          <div class="col tab2">
-            <?php if($post->post_content != "") : ?>
-              <a href="#" class="trigger"></a>
-              <?php else: ?>
-              <a href="#"></a>
-              <?php endif; ?>
-          </div>
-          <div class="col tab50">
-              <p class="title"><?php the_title(); ?></p>
-          </div>
-          <div class="col tab5">
-            <p>Aktuell</p>
-          </div>
-          <div class="col tab15">
-              <p><?php the_field('location'); ?></p>
-          </div>
-          <div class="col tab28">
-            <ul class="post-categories">
-              <li>
-            <?php if( get_field('divsions') ): ?>
-                  <?php the_field('divsions'); ?>
-            <?php endif; ?>
-              </li>
-            </ul>
-          </div>
-          </div>
-              <div class="toggle">
-               <?php if( get_field('factsfigures') ): ?>
-                    <?php the_field('factsfigures'); ?>
-                <?php endif; ?>
-              </div>
-          </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>  
-          </div> 
-
-          <?php query_posts( array( 'category__in' => array(60,11,42,95), 'posts_per_page' => -1, 'orderby' => 'date', 'order' => 'DESC', 'post__not_in' => array(73,2141,225,1605,1637,1844))); ?>
-          <?php while (have_posts()) : the_post(); ?>
-          <div class="factbox">
-          <div class="factsheader">
-          <div class="col tab2">
-              <?php if( get_field('factsfigures') ): ?>
+          
+          <table class="factbox">
+            <tr class="factsheader">
+              <td class="tab2">
+                <?php if($post->post_content != "") : ?>
                   <a href="#" class="trigger"></a>
-              <?php else: ?>
-              <a href="#"></a>
-              <?php endif; ?>
-          </div>
-          <div class="col tab50">
-              <p class="title"><?php the_title(); ?></p>
-          </div>
-          <div class="col tab5">
-            <p><?php the_date('Y'); ?></p>
-          </div>
-          <div class="col tab15">
-              <p><?php the_field('location'); ?></p>
-          </div>
-          <div class="col tab28">
-            <ul class="post-categories">
-              <li>
-            <?php if( get_field('divsions') ): ?>
-                  <?php the_field('divsions'); ?>
-            <?php endif; ?>
-              </li>
-            </ul>
-          </div>
-          </div>
-   
-              <div class="toggle">
-               <?php if( get_field('factsfigures') ): ?>
+                <?php else: ?>
+                  <a href="#"></a>
+                <?php endif; ?>
+              </td>
+              <td class="tab50">
+                <p class="title"><?php the_title(); ?></p>
+              </td>
+              <td class="tab5">
+                <p>Aktuell</p>
+              </td>
+              <td class="tab15">
+                <p><?php the_field('location'); ?></p>
+              </td>
+              <td class="tab28">
+                <ul class="post-categories">
+                  <?php if( get_field('divsions') ): ?>
+                    <?php the_field('divsions'); ?>
+                  <?php endif; ?>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td class="toggle">
+                <?php if( get_field('factsfigures') ): ?>
                     <?php the_field('factsfigures'); ?>
                 <?php endif; ?>
-              </div>
-          </div>
+              </td>
+            </tr>
+          </table>
+
           <?php endwhile; ?>
           <?php wp_reset_query(); ?>  
-          </div> 
-
-
 
     </div>
     </div>
