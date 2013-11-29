b5e57cadce9e7cc9f769fd1316ca5698d0caa3a3
Chad Danford <chad.m.danford@gmail.com>
2013-11-29 09:58:39 +0100

    role main

--- profileundpotenzial.php
+++ profileundpotenzial.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: profile+potenzial */ get_header(); ?>
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <section role="main">
-    <div class="col megacontent"main>
+    <div class="col maincontent" role="main">
 
       <div class="col col100">
         <div class="module">
