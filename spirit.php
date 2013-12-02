75423375d7f041e3cb1a1efdd6573c0da1a95535
Chad Danford <chad.m.danford@gmail.com>
2013-12-02 16:23:11 +0100

    test

---
+++ spirit.php
@@ -0,0 +1,31 @@
+<?php /* Template Name: spirit */ get_header(); ?>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+  <section role="main">
+    <div class="col maincontent"main>
+      
+      <div class="col col100">
+        <div class="module">
+          <div class="textwrap categorysingle">
+            <div class="singletitle">
+              <h2><?php the_title(); ?>
+                <?php if( get_field('subheadline') ): ?>
+                 <span><?php the_field('subheadline'); ?></span></h2>
+                <?php endif; ?>
+               </div>   
+
+               <div class="factbox" style="margin-bottom: 30px;padding-left:0;" id="<?php the_title(); ?>">
+                <?php wp_nav_menu( array('menu' => 'spirit' )); ?>        
+              </div>
+
+            <?php the_content(); ?>   
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sidebar">
+      <?php get_sidebar(); ?>
+    </div>
+  </section>
+  <?php endwhile; ?>
+  <?php endif; ?>
+<?php get_footer(); ?>
