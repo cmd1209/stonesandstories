7a3019283ad3b9e408b387c1f93708536546840f
Chad Danford <chad.m.danford@gmail.com>
2013-12-03 16:27:01 +0100

    test

--- search.php
+++ search.php
@@ -4,27 +4,32 @@
 	<section role="main">
 
 <div class="col maincontent">
-		<div class="col col100" style="min-height:800px;">
+		<div class="col col100">
 			<div class="module">
 				<h1><strong><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 				<div class="col col100">
 
 					 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-						<div class="textwrap">
-							<div class="singletitle">
-								<h2><?php the_title(); ?>
+						<div class="textwrap" style="min-height:800px;">
+							<div class="col30">
+								<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
+									<?php the_post_thumbnail('large'); ?>
+								</a>								
+							</div>
+							<div class="col70">
+							 <div class="singletitle">
+							   <h2><?php the_title(); ?>
 									<?php if( get_field('subheadline') ): ?>
 										<span><?php the_field('subheadline'); ?></span>
 							<?php endif; ?>
 									</h2>
 								</div>
-							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
-								
-									<?php the_post_thumbnail('large'); ?>
-								</a>
+						
 									<?php echo excerpt(35); ?>
 								</div>
+								
+							</div>
+
 						<?php endwhile; ?>
 					<?php endif; ?>
 					</div>
