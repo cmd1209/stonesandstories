bc820c600cf9cb9a59a7143f426db72b7e02f40c
Chad Danford <chad.m.danford@gmail.com>
2013-12-03 15:04:26 +0100

    red

--- start.php
+++ start.php
@@ -19,7 +19,7 @@
                   <?php echo excerpt(70); ?>
                 </div>              
             </div>
-            <div class="col70">
+            <div class="col70" style="background-color:red;">
               <figure><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" style="height: 100%; width:auto;" alt=""></figure>
             </div>
             <?php endwhile; ?>
