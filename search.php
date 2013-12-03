5f827b7e722a2db2c1bbebaa4b9a4d9d7688a889
Chad Danford <chad.m.danford@gmail.com>
2013-12-03 16:28:13 +0100

    asdfas

--- search.php
+++ search.php
@@ -9,14 +9,14 @@
 				<h1><strong><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 				<div class="col col100">
 
-					 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 						<div class="textwrap" style="min-height:800px;">
-							<div class="col30">
+					 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+							<div class="col50">
 								<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
 									<?php the_post_thumbnail('large'); ?>
 								</a>								
 							</div>
-							<div class="col70">
+							<div class="col50">
 							 <div class="singletitle">
 							   <h2><?php the_title(); ?>
 									<?php if( get_field('subheadline') ): ?>
