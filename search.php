c15d7510dcb4f8a896d6be1d9f63f658c714e1dc
Chad Danford <chad.m.danford@gmail.com>
2013-12-03 16:18:32 +0100

    test

--- search.php
+++ search.php
@@ -4,12 +4,12 @@
 	<section role="main">
 
 <div class="col maincontent">
-		<div class="col col100">
+		<div class="col col100" style="min-height:800px;">
 			<div class="module">
 				<h1><strong><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
-				<div class="col col50">
+				<div class="col col100">
 
-					<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) == 0) : $wp_query->next_post(); else : the_post(); ?>
+					 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 						<div class="textwrap">
 							<div class="singletitle">
@@ -20,53 +20,14 @@
 									</h2>
 								</div>
 							<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
-								<?php
-								if (class_exists('MultiPostThumbnails')) :
-									if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-										MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-									else : ?>
+								
 									<?php the_post_thumbnail('large'); ?>
-									<?php
-									endif;
-									endif;
-									?></a>
+								</a>
 									<?php echo excerpt(35); ?>
 								</div>
-
-							<?php endif; endwhile; else: ?>
-							<div>Alternate content</div>
 						<?php endif; ?>
+					<?php endwhile; ?>
 					</div>
-					<?php $i = 0; rewind_posts(); ?>
-					<div class="col col50">
-						<?php if (have_posts()) : while(have_posts()) : $i++; if(($i % 2) !== 0) : $wp_query->next_post(); else : the_post(); ?>
-
-							<div class="textwrap">
-								<div class="singletitle">
-								<h2><?php the_title(); ?>
-									<?php if( get_field('subheadline') ): ?>
-										<span><?php the_field('subheadline'); ?></span>
-								<?php endif; ?>
-									</h2>
-								</div>
-								<a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title(); ?>">
-									<?php
-									if (class_exists('MultiPostThumbnails')) :
-										if ( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'secondary-image', NULL, 'secondary-image') ) :
-											MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'secondary-image', NULL, 'secondary-image');
-										else : ?>
-										<?php the_post_thumbnail('large'); ?>
-										<?php
-										endif;
-										endif;
-										?></a>
-										<p><?php echo excerpt(35); ?></p>
-									</div>
-
-								<?php endif; endwhile; else: ?>
-								<div>Alternate content</div>
-							<?php endif; ?>
-						</div>
 					</div>
 				</div>
 
