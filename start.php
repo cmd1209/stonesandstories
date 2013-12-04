e18ac289fd20f8853532f33cc50dd6b3e71ce249
Chad Danford <chad.m.danford@gmail.com>
2013-12-04 12:50:24 +0100

    link

--- start.php
+++ start.php
@@ -20,7 +20,7 @@
                 </div>              
             </div>
             <div class="col70">
-              <figure><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" alt=""></figure>
+              <a class="thumbnail" href="<?php echo get_permalink(); ?>"><figure><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" alt=""></figure></a>
             </div>
             <?php endwhile; ?>
           </div>
