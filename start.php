8d0b3193fa2cf5b649433256db74f002e38a9dea
Chad Danford <chad.m.danford@gmail.com>
2013-12-09 11:45:38 +0100

    asdfasd

--- start.php
+++ start.php
@@ -20,7 +20,7 @@
                 </div>              
             </div>
             <div style="width:70%;float:left;position:relative;display:block;background-color:red;">
-                          <img style="position:absolute;margin-left:-50%;left:50%;"src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" alt="">
+
 
             </div>
             <?php endwhile; ?>
