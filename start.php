d48c4b3f10886c7ce976db57c5ee5dc825cd0241
Chad Danford <chad.m.danford@gmail.com>
2013-12-09 11:13:07 +0100

    adfasd

--- start.php
+++ start.php
@@ -19,8 +19,8 @@
                   <?php echo excerpt(70); ?>
                 </div>              
             </div>
-            <div class="col70" style="position:relative;">
-              <img style="position:absolute;margin-left:-50%;left:50%;" height="100%" width="auto" src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" alt="">
+            <div class="col70" style="position:relative;height:100%;background-color:red;">
+              <img style="position:absolute;margin-left:-50%;left:50%;height:100%!important;width:auto!important;" height="100%" width="auto" src="<?php echo home_url(); ?>/wp-content/uploads/2013/11/EHM-Wand.jpg" alt="">
             </div>
             <?php endwhile; ?>
           </div>
