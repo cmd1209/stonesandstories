610a6f4f33d708f0cf2be93d2aa631455f42b76e
Chad Danford <chad.m.danford@gmail.com>
2016-10-27 16:02:57 +0200

    search submit form

--- header.php
+++ header.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-    
+
     <!-- dns prefetch -->
     <link href="//www.google-analytics.com" rel="dns-prefetch">
     <link href="//fonts.googleapis.com/css?family=Arimo:400italic,400,700italic,700" rel="stylesheet" type="text/css">
-   
+    <script src="https://use.fontawesome.com/75215c3005.js"></script>
+
     <!-- meta -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
@@ -36,7 +37,7 @@
             <a class="mobilenavtrigger" href="#mobilenav">&#9776;</a>
             <a href="<?php echo home_url(); ?>">
               <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-            </a> 
+            </a>
               <?php get_template_part('searchform'); ?>
         </div>
         <div class="navigation">
--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 <form method="get" action="<?php echo home_url(); ?>" role="search">
 <input class="sb-search-input" placeholder="suchen ..." type="text" value="" name="s" id="search">
 <input class="sb-search-submit" type="submit" value="">
-<span class="sb-icon-search">&#x1f50d;</span>
+<span class="sb-icon-search"><i class="fa fa-search"></i></span>
 </form>
 </div>
 <!-- /search -->
